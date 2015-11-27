3e43dea1846c77ac58898dd7981ebf360139a386
Durga Prasana <durga.s@flipkart.com>
2015-11-27 15:14:29 +0530

    api v1 basepath

--- receptors/src/main/scala/com/flipkart/connekt/receptors/routes/pn/Registration.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/routes/pn/Registration.scala
@@ -23,57 +23,53 @@ class Registration (implicit _am: ActorMaterializer) extends BaseHandler {
   val register =
     sniffHeaders { headers =>
       isAuthenticated(Some(headers)) {
-        pathPrefix("push") {
-          pathPrefix("device") {
-            pathPrefix("registration") {
-              path("save") {
-                (post | put) {
-                  entity(as[DeviceDetails]) { d =>
-                    def save = DaoFactory.getDeviceDetailsDao.saveDeviceDetails(d)
-                    async(save) {
-                      case Success(t) =>
-                        complete(respond[GenericResponse](
-                          StatusCodes.Created, Seq.empty[HttpHeader],
-                          GenericResponse("DeviceDetails registered for %s".format(d.deviceId), null, null, StatusCodes.Created.intValue)
-                        ))
-                      case Failure(e) =>
-                        complete(respond[GenericResponse](
-                          StatusCodes.InternalServerError, Seq.empty[HttpHeader],
-                          GenericResponse("DeviceDetails registration failed for %s".format(d.deviceId), e.getMessage, null , StatusCodes.InternalServerError.intValue)
-                        ))
-                    }
+        pathPrefix("v1" / "push" / "device" / "registration") {
+          path("save") {
+            (post | put) {
+              entity(as[DeviceDetails]) { d =>
+                def save = DaoFactory.getDeviceDetailsDao.saveDeviceDetails(d)
+                async(save) {
+                  case Success(t) =>
+                    complete(respond[GenericResponse](
+                      StatusCodes.Created, Seq.empty[HttpHeader],
+                      GenericResponse("DeviceDetails registered for %s".format(d.deviceId), null, null, StatusCodes.Created.intValue)
+                    ))
+                  case Failure(e) =>
+                    complete(respond[GenericResponse](
+                      StatusCodes.InternalServerError, Seq.empty[HttpHeader],
+                      GenericResponse("DeviceDetails registration failed for %s".format(d.deviceId), e.getMessage, null , StatusCodes.InternalServerError.intValue)
+                    ))
+                }
+              }
+            }
+          } ~
+            path("fetch" / Segment / Segment) {
+              (appName: String, deviceId: String) =>
+                get {
+                  def fetch = DaoFactory.getDeviceDetailsDao.fetchDeviceDetails(appName, deviceId)
+                  async(fetch) {
+                    case Success(resultOption) =>
+                      resultOption match {
+                        case Some(deviceDetails) =>
+                          complete(respond[GenericResponse](
+                            StatusCodes.OK, Seq.empty[HttpHeader],
+                            GenericResponse("DeviceDetails fetched for app: %s %s".format(appName, deviceId), null, deviceDetails, StatusCodes.OK.intValue)
+                          ))
+                        case None =>
+                          complete(respond[GenericResponse](
+                            StatusCodes.OK, Seq.empty[HttpHeader],
+                            GenericResponse("No DeviceDetails found for app:%s %s".format(appName, deviceId), null, null, StatusCodes.OK.intValue)
+                          ))
+
+                      }
+                    case Failure(error) =>
+                      complete(respond[GenericResponse](
+                        StatusCodes.InternalServerError, Seq.empty[HttpHeader],
+                        GenericResponse("Fetching DeviceDetails failed for app:%s %s".format(appName, deviceId), error.getMessage, null, StatusCodes.InternalServerError.intValue)
+                      ))
                   }
                 }
-              } ~
-                path("fetch" / Segment / Segment) {
-                  (appName: String, deviceId: String) =>
-                    get {
-                      def fetch = DaoFactory.getDeviceDetailsDao.fetchDeviceDetails(appName, deviceId)
-                      async(fetch) {
-                        case Success(resultOption) =>
-                          resultOption match {
-                            case Some(deviceDetails) =>
-                              complete(respond[GenericResponse](
-                                StatusCodes.OK, Seq.empty[HttpHeader],
-                                GenericResponse("DeviceDetails fetched for app: %s %s".format(appName, deviceId), null, deviceDetails, StatusCodes.OK.intValue)
-                              ))
-                            case None =>
-                              complete(respond[GenericResponse](
-                                StatusCodes.OK, Seq.empty[HttpHeader],
-                                GenericResponse("No DeviceDetails found for app:%s %s".format(appName, deviceId), null, null, StatusCodes.OK.intValue)
-                              ))
-
-                          }
-                        case Failure(error) =>
-                          complete(respond[GenericResponse](
-                            StatusCodes.InternalServerError, Seq.empty[HttpHeader],
-                            GenericResponse("Fetching DeviceDetails failed for app:%s %s".format(appName, deviceId), error.getMessage, null, StatusCodes.InternalServerError.intValue)
-                          ))
-                      }
-                    }
-                }
             }
-          }
         }
       }
     }
