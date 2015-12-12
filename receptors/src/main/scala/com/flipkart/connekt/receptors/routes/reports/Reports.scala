60dbe256d69a277c5899109c6451f0e745739eb4
Durga Prasana <durga.s@flipkart.com>
2015-12-13 03:54:46 +0530

    cleanup

--- receptors/src/main/scala/com/flipkart/connekt/receptors/routes/reports/Reports.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/routes/reports/Reports.scala
@@ -2,12 +2,11 @@ package com.flipkart.connekt.receptors.routes.reports
 
 import akka.http.scaladsl.model.{HttpHeader, StatusCodes}
 import akka.stream.ActorMaterializer
-import com.flipkart.connekt.commons.factories.{ConnektLogger, LogFile, ServiceFactory}
+import com.flipkart.connekt.commons.factories.ServiceFactory
 import com.flipkart.connekt.commons.iomodels._
 import com.flipkart.connekt.receptors.routes.BaseHandler
 
 import scala.collection.immutable.Seq
-import scala.util.{Failure, Success}
 
 /**
  *
@@ -22,23 +21,15 @@ class Reports(implicit am: ActorMaterializer) extends BaseHandler {
       authenticate {
         user =>
           path(Segment / "events" / Segment / Segment) {
-          (channel: String, contactId: String, messageId: String) =>
-            get {
-              ServiceFactory.getCallbackService.fetchCallbackEvent(messageId, contactId, channel) match {
-                case Success(events) =>
-                  ConnektLogger(LogFile.SERVICE).info(s"Received callback events for $messageId $contactId: ${events}")
-                  complete(respond[GenericResponse](
+            (channel: String, contactId: String, messageId: String) =>
+              get {
+                val events = ServiceFactory.getCallbackService.fetchCallbackEvent(messageId, contactId, channel).get
+                complete(respond[GenericResponse](
                   StatusCodes.OK, Seq.empty[HttpHeader],
                   GenericResponse(StatusCodes.OK.intValue, null, Response(s"Events for $messageId $contactId fetched.", Map(contactId -> events)))
-                  ))
-                case Failure(t) =>
-                  complete(respond[GenericResponse](
-                    StatusCodes.InternalServerError, Seq.empty[HttpHeader],
-                    GenericResponse(StatusCodes.OK.intValue, null, Response(s"Events fetch failed for $messageId $contactId.", null))
-                  ))
+                ))
               }
-            }
-        }
+          }
       }
     }
 }
