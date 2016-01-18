b2c4879fb6e7e977fb5dae44897d161134147edc
Kinshuk Bairagi <me@kinshuk.in>
2016-01-19 03:34:26 +0530

    8087 changes

--- 8087/src/main/java/com/flipkart/marketing/connekt/RegistryCoprocessor.java
+++ 8087/src/main/java/com/flipkart/marketing/connekt/RegistryCoprocessor.java
@@ -1,6 +1,8 @@
 package com.flipkart.marketing.connekt;
 
 import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.CoprocessorEnvironment;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Put;
@@ -8,19 +10,10 @@ import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
 import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
+import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
-
-import org.apache.hadoop.hbase.CoprocessorEnvironment;
-import org.apache.hadoop.hbase.client.Delete;
-import org.apache.hadoop.hbase.client.Durability;
-import org.apache.hadoop.hbase.client.Put;
-import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
-import org.apache.hadoop.hbase.coprocessor.ObserverContext;
-import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
-import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
-import org.apache.hadoop.hbase.util.Bytes;
-import org.apache.commons.logging.LogFactory;
+import java.util.Arrays;
 
 /**
  * Created by kinshuk.bairagi on 19/01/16.
@@ -30,26 +23,31 @@ public class RegistryCoprocessor extends BaseRegionObserver {
     private static final Log logger = LogFactory.getLog(RegistryCoprocessor.class);
 
     @Override
+    public void start(CoprocessorEnvironment env) throws IOException {
+        logger.info("RegistryCoprocessor start");
+    }
+
+    @Override
     public void preDelete(ObserverContext<RegionCoprocessorEnvironment> e, Delete delete, WALEdit edit, boolean writeToWAL) throws IOException {
         super.preDelete(e, delete, edit, writeToWAL);
     }
 
+
     @Override
-    public void postPut(ObserverContext<RegionCoprocessorEnvironment> e, Put put, WALEdit edit, boolean writeToWAL) throws IOException {
-
+    public void prePut(ObserverContext<RegionCoprocessorEnvironment> contx, Put put, WALEdit edit, boolean writeToWAL) throws IOException {
         try {
-            KeyValue kv = new KeyValue(Bytes.toBytes("COPROCESSORROW"), Bytes.toBytes("colfam1"),Bytes.toBytes("COPROCESSOR: "+System.currentTimeMillis()),Bytes.toBytes("IT WORKED"));
-            //put.add(kv);
+            byte[] tableName = contx.getEnvironment().getRegion().getTableDesc().getName();
+            String tableNameStr = Bytes.toString(tableName);
+            if(Arrays.asList(RegistryConfig.TABLES).contains(tableNameStr)){
 
-//            String indexId = new String(put);
-            //Object d = put.getRow();
 
-            logger.info("werty");
+                logger.info("werty");
+            }
         } catch (Exception ex) {
             logger.error(ex);
         }
 
-        super.postPut(e, put, edit, writeToWAL);
+        super.postPut(contx, put, edit, writeToWAL);
     }
 }
 
