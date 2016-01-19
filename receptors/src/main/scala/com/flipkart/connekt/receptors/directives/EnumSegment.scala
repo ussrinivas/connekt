d0e015172a96f08919005cd9ec2804b8ec31dc1b
Kinshuk Bairagi <me@kinshuk.in>
2016-01-19 17:40:09 +0530

    Enum segment changes

--- receptors/src/main/scala/com/flipkart/connekt/receptors/directives/EnumSegment.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/directives/EnumSegment.scala
@@ -3,20 +3,19 @@ package com.flipkart.connekt.receptors.directives
 import akka.http.scaladsl.model.Uri.Path
 import akka.http.scaladsl.server.PathMatcher.{Matched, Unmatched}
 import akka.http.scaladsl.server.PathMatcher1
-
+import com.flipkart.connekt.commons.entities.Channel
+import com.flipkart.connekt.commons.entities.Channel.Channel
 
 /**
  * @author aman.shrivastava on 18/01/16.
  */
 
-abstract class EnumSegment[T <: Enumeration#Value](clz : String)(implicit manifest : Manifest[T]) extends PathMatcher1[T] {
-
+abstract class EnumSegment[T <: Enumeration#Value](clz : Class[_])(implicit manifest : Manifest[T]) extends PathMatcher1[T] {
 
   def apply(path: Path) = path match {
     case Path.Segment(segment, tail) ⇒
       try{
-
-        val method = Class.forName(clz).getMethod("withName", classOf[String])
+        val method = Class.forName(clz.getEnumClassName).getMethod("withName", classOf[String])
         method.setAccessible(true)
         val value = method.invoke(null, segment).asInstanceOf[T]
         Matched(tail, Tuple1(value))
@@ -27,10 +26,14 @@ abstract class EnumSegment[T <: Enumeration#Value](clz : String)(implicit manife
     case _ ⇒
       Unmatched
   }
+
+  implicit class enumClassExtractor(val clz: Class[_]) {
+    def getEnumClassName  = clz.getName.stripSuffix("$")
+  }
+
 }
 
-object ChannelSegment  extends EnumSegment[com.flipkart.connekt.commons.entities.Channel.Value]("com.flipkart.connekt.commons.entities.Channel")
 
-//object ChannelSegment  extends EnumSegment[com.flipkart.connekt.commons.entities.Channel.Value](Channel.getClass.getName)
+object ChannelSegment  extends EnumSegment[Channel](Channel.getClass)
 
 
