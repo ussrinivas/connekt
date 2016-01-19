d20171ce3c6785a3b1c6e78b37c8ae591e42e5d9
Durga Prasana <durga.s@flipkart.com>
2016-01-19 18:27:42 +0530

    co-processor impl for sec idx

--- 8087/src/main/java/com/flipkart/marketing/connekt/RegistryCoprocessor.java
+++ 8087/src/main/java/com/flipkart/marketing/connekt/RegistryCoprocessor.java
@@ -5,22 +5,29 @@ import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.hbase.CoprocessorEnvironment;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.HTableInterface;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.coprocessor.BaseRegionObserver;
 import org.apache.hadoop.hbase.coprocessor.ObserverContext;
 import org.apache.hadoop.hbase.coprocessor.RegionCoprocessorEnvironment;
 import org.apache.hadoop.hbase.regionserver.wal.WALEdit;
+import org.apache.hadoop.hbase.rest.protobuf.generated.CellMessage;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by kinshuk.bairagi on 19/01/16.
  */
-public class RegistryCoprocessor extends BaseRegionObserver {
+public class RegistryCoprocessor extends BaseRegionObserver implements RegistryConfig {
 
     private static final Log logger = LogFactory.getLog(RegistryCoprocessor.class);
+    private static final ExecutorService execService = Executors.newCachedThreadPool();
 
     @Override
     public void start(CoprocessorEnvironment env) throws IOException {
@@ -34,20 +41,62 @@ public class RegistryCoprocessor extends BaseRegionObserver {
 
 
     @Override
-    public void prePut(ObserverContext<RegionCoprocessorEnvironment> contx, Put put, WALEdit edit, boolean writeToWAL) throws IOException {
-        try {
-            byte[] tableName = contx.getEnvironment().getRegion().getTableDesc().getName();
-            String tableNameStr = Bytes.toString(tableName);
-            if(Arrays.asList(RegistryConfig.TABLES).contains(tableNameStr)){
+    public void prePut(ObserverContext<RegionCoprocessorEnvironment> ctx, Put put, WALEdit edit, boolean writeToWAL) throws IOException {
 
-
-                logger.info("werty");
-            }
-        } catch (Exception ex) {
-            logger.error(ex);
+        String tableName = Bytes.toString(ctx.getEnvironment().getRegion().getRegionInfo().getTableName());
+        if(tableName.equals(TABLE_REGISTRATION)) {
+            Put userIdSecIdx = putSecondaryIdx(ctx, put, TABLE_USER_ID_SEC_IDX, COL_Q_USER_ID, COL_Q_DEVICE_ID);
+            if(null != userIdSecIdx)
+                if(null == putSecondaryIdx(ctx, put, TABLE_TOKEN_SEC_IDX, COL_Q_TOKEN, COL_Q_DEVICE_ID)) {
+                    removeInconsistentIdx(ctx, userIdSecIdx.getRow());
+                    throw new RuntimeException("Adding secondary index failed for " + new String(put.getRow()));
+                }
         }
+    }
 
-        super.postPut(contx, put, edit, writeToWAL);
+    private Put putSecondaryIdx(ObserverContext<RegionCoprocessorEnvironment> ctx, Put registration, String secIndexTable, String secColQualifier, String primaryColQualifier) {
+
+        HTableInterface hTable = null;
+        try {
+            byte[] secColValue = registration.get(COL_F_REGISTRATION.getBytes(StandardCharsets.UTF_8), secColQualifier.getBytes(StandardCharsets.UTF_8)).get(0).getValue();
+            byte[] primaryColValue = registration.get(COL_F_REGISTRATION.getBytes(StandardCharsets.UTF_8), primaryColQualifier.getBytes(StandardCharsets.UTF_8)).get(0).getValue();
+
+            Put secPut = new Put(Utils.shaHash(secColValue));
+            secPut.add(COL_F_SEC_IDX.getBytes(StandardCharsets.UTF_8), primaryColQualifier.getBytes(StandardCharsets.UTF_8), primaryColValue);
+            hTable = ctx.getEnvironment().getTable(TABLE_REGISTRATION.getBytes(), execService);
+            hTable.put(secPut);
+            return secPut;
+
+        } catch (Exception e) {
+            logger.error(String.format("Secondary Idx[%s] Put Failed: %s", secIndexTable, e.getMessage()), e);
+        } finally {
+            if(null != hTable)
+                try {
+                    hTable.close();
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+        }
+        return null;
+    }
+
+    private void removeInconsistentIdx(ObserverContext<RegionCoprocessorEnvironment> ctx, byte[] rowKey) {
+
+        HTableInterface hTable = null;
+        try {
+            hTable = ctx.getEnvironment().getTable(TABLE_REGISTRATION.getBytes(), execService);
+            hTable.delete(new Delete(rowKey));
+
+        } catch (Exception e) {
+            logger.error("Inconsistent Secondary Idx removal failed: " + e.getMessage(), e);
+        } finally {
+            if(null != hTable)
+                try {
+                    hTable.close();
+                } catch (IOException e) {
+                    logger.error(e.getMessage(), e);
+                }
+        }
     }
 }
 
