ded11f19305a4237c67894c75f0351e9360aaac2
Kinshuk Bairagi <me@kinshuk.in>
2016-02-02 15:10:25 +0530

    CNKT-01 Adding rate-limiter flows

---
+++ busybees/src/main/scala/com/flipkart/connekt/busybees/streams/sources/RateControl.scala
@@ -0,0 +1,49 @@
+package com.flipkart.connekt.busybees.streams.sources
+
+import java.util.concurrent.TimeUnit
+
+import akka.stream._
+import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import com.flipkart.connekt.commons.factories.{ConnektLogger, LogFile}
+import org.isomorphism.util.TokenBuckets
+
+import scala.reflect.ClassTag
+
+/**
+ * Created by kinshuk.bairagi on 02/02/16.
+ *
+ * @param capacity
+ * @param tokenRefreshPeriod
+ * @param tokenRefreshAmount
+ * @tparam V
+ */
+class RateControl[V: ClassTag](capacity: Long, tokenRefreshPeriod: Long, tokenRefreshAmount: Long) extends GraphStage[FlowShape[V, V]] {
+
+  val in = Inlet[V]("RateControl.In")
+  val out = Outlet[V]("RateControl.Out")
+
+  override def shape: FlowShape[V, V] = FlowShape.of(in, out)
+
+  val tokenBucket = TokenBuckets.builder().withCapacity(capacity)
+    .withFixedIntervalRefillStrategy(tokenRefreshAmount, tokenRefreshPeriod, TimeUnit.SECONDS).build()
+
+  override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+
+    setHandler(in, new InHandler {
+      override def onPush(): Unit = {
+        if (tokenBucket.tryConsume(1)) {
+          val message = grab(in)
+          push(out, message)
+        } else {
+          ConnektLogger(LogFile.PROCESSORS).warn("Rate Limited..")
+        }
+      }
+    })
+
+    setHandler(out, new OutHandler {
+      override def onPull(): Unit = pull(in)
+    })
+
+  }
+
+}
