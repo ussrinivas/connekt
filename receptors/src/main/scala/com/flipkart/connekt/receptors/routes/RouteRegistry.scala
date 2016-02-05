75210a6cb494965e00010db6dd025cd18a2a5aa7
aman.shrivastava <aman.shrivastava@flipkart.com>
2016-02-05 14:47:48 +0530

    CNKT-001: route registry clean up.

--- receptors/src/main/scala/com/flipkart/connekt/receptors/routes/RouteRegistry.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/routes/RouteRegistry.scala
@@ -7,15 +7,25 @@ import com.flipkart.connekt.receptors.routes.Stencils.StencilsRoute
 import com.flipkart.connekt.receptors.routes.callbacks.Callback
 import com.flipkart.connekt.receptors.routes.push.{Fetch, LdapAuthentication, Registration, Unicast}
 import com.flipkart.connekt.receptors.routes.reports.Reports
+
 /**
  * Created by kinshuk.bairagi on 10/12/15.
  */
-class RouteRegistry(implicit mat:ActorMaterializer) extends AuthenticationDirectives {
+class RouteRegistry(implicit mat: ActorMaterializer) extends AuthenticationDirectives {
 
- def allRoutes = authenticate {
-  implicit user => {
-   new Unicast().unicast ~ new Registration().register ~ new Callback().callback ~ new Reports().route ~ new Fetch().fetch ~ new StencilsRoute().stencils ~ new LdapAuthentication().token
+  def allRoutes = authenticate {
+    implicit user => {
+
+      val receptorReqHandler = new Registration().register
+      val unicastHandler = new Unicast().unicast
+      val callbackHandler = new Callback().callback
+      val reportsRoute = new Reports().route
+      val fetchRoute = new Fetch().fetch
+      val stencilRoute = new StencilsRoute().stencils
+      val ldapRoute = new LdapAuthentication().token
+
+      unicastHandler ~ receptorReqHandler ~ callbackHandler ~ reportsRoute ~ fetchRoute ~ stencilRoute ~ ldapRoute
+    }
   }
- }
 
 }
