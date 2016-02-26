20ede1ce33a23f5d4e6bfb2a8133eec2bdae1b83
Kinshuk Bairagi <me@kinshuk.in>
2016-02-27 02:28:21 +0530

    Separate out access log from akka logs, log http response times.

--- commons/src/main/scala/com/flipkart/connekt/commons/services/BigfootService.scala
+++ commons/src/main/scala/com/flipkart/connekt/commons/services/BigfootService.scala
@@ -16,7 +16,7 @@ import scala.util.{Failure, Success, Try}
 /**
  * Created by nidhi.mehla on 02/02/16.
  */
-object BigfootService extends Instrumented{
+object BigfootService extends Instrumented {
 
   val socketClient = DaoFactory.phantomClientSocket
 
@@ -25,33 +25,33 @@ object BigfootService extends Instrumented{
   @Timed("ingest")
   def ingest(obj: BaseSchema): Try[Boolean] = {
 
-    if (ingestionEnabled ) {
+    if (ingestionEnabled) {
 
-    val eventId = StringUtils.generateRandomStr(25)
-    val eventTime = System.currentTimeMillis()
-    val event = new Event(eventId, eventTime.toString, obj)
-    val ingestionMetadata: IngestionMetadata = new IngestionMetadata()
-    ingestionMetadata.setRequestId(eventId)
-    val request: SpecterRequest = new SpecterRequest(event, ingestionMetadata)
+      val eventId = StringUtils.generateRandomStr(25)
+      val eventTime = System.currentTimeMillis()
+      val event = new Event(eventId, eventTime.toString, obj)
+      val ingestionMetadata: IngestionMetadata = new IngestionMetadata()
+      ingestionMetadata.setRequestId(eventId)
+      val request: SpecterRequest = new SpecterRequest(event, ingestionMetadata)
 
-    try {
-      SpecterClient.sendToSpecter(socketClient, request).getResponse match {
-        case "SUCCESS" =>
-          Success(true)
-        case commandResponse: String =>
-          ConnektLogger(LogFile.SERVICE).error(s"UnSuccessful Ingestion, FAILED_TO_INGEST : $commandResponse")
-          Failure(new Exception(s"Unsuccessful specter response: $commandResponse"))
-      }
-    } catch {
-      case e: PhantomClientException =>
-        ConnektLogger(LogFile.SERVICE).error("Specter connection Error, FAILED_TO_INGEST", e)
-        Failure(e)
-      case ie: InterruptedException =>
-        ConnektLogger(LogFile.SERVICE).error("Interrupt Exception , FAILED_TO_INGEST", ie)
-        Failure(ie)
-      case e: Exception =>
-        ConnektLogger(LogFile.SERVICE).error("Unknown ERROR, FAILED_TO_INGEST", e)
-        Failure(e)
+      try {
+        SpecterClient.sendToSpecter(socketClient, request).getResponse match {
+          case "SUCCESS" =>
+            Success(true)
+          case commandResponse: String =>
+            ConnektLogger(LogFile.SERVICE).error(s"UnSuccessful Ingestion, FAILED_TO_INGEST : $commandResponse")
+            Failure(new Exception(s"Unsuccessful specter response: $commandResponse"))
+        }
+      } catch {
+        case e: PhantomClientException =>
+          ConnektLogger(LogFile.SERVICE).error("Specter connection Error, FAILED_TO_INGEST", e)
+          Failure(e)
+        case ie: InterruptedException =>
+          ConnektLogger(LogFile.SERVICE).error("Interrupt Exception , FAILED_TO_INGEST", ie)
+          Failure(ie)
+        case e: Exception =>
+          ConnektLogger(LogFile.SERVICE).error("Unknown ERROR, FAILED_TO_INGEST", e)
+          Failure(e)
       }
     } else {
       // Ingestion disabled.
