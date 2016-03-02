93ec75c41aa934f528f4e9d7dc78be9a40b9e644
Kinshuk Bairagi <me@kinshuk.in>
2016-03-02 18:26:06 +0530

    SyncType Json Handle

--- commons/src/main/scala/com/flipkart/connekt/commons/sync/SyncMessage.scala
+++ commons/src/main/scala/com/flipkart/connekt/commons/sync/SyncMessage.scala
@@ -1,8 +1,12 @@
 package com.flipkart.connekt.commons.sync
 
+import com.fasterxml.jackson.databind.annotation.{JsonDeserialize, JsonSerialize}
 import com.flipkart.connekt.commons.sync.SyncType.SyncType
 
 /**
  * Created by kinshuk.bairagi on 02/02/16.
  */
-case class SyncMessage(topic: SyncType, message: List[AnyRef])
+case class SyncMessage(@JsonSerialize(using = classOf[SyncTypeToStringSerializer])
+                       @JsonDeserialize(using = classOf[SyncTypeToStringDeserializer])
+                       topic: SyncType,
+                       message: List[AnyRef])
