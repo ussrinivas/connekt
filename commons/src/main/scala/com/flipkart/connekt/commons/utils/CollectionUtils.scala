77adbc549fb7b2919502271f489cef4e3f596364
Durga Prasana <durga.s@flipkart.com>
2016-03-12 00:34:51 +0530

    refactor iterator merge

--- commons/src/main/scala/com/flipkart/connekt/commons/utils/CollectionUtils.scala
+++ commons/src/main/scala/com/flipkart/connekt/commons/utils/CollectionUtils.scala
@@ -17,25 +17,23 @@ object CollectionUtils {
 
       private var inUseItrIndex = 0
 
+      def unsafeHasNext(index: Int) = try { iterators(index).hasNext } catch { case e: ConsumerTimeoutException => false }
+
       override def hasNext: Boolean = {
         if (iterators.nonEmpty) {
           inUseItrIndex = (1 + inUseItrIndex) % iterators.size
 
-          def doesHaveNext(index: Int) = try { iterators(index).hasNext } catch { case e: ConsumerTimeoutException => false }
-
-          def doOthersHaveNext() = {
+          unsafeHasNext(inUseItrIndex) || {
             def r(stopIdx: Int): Boolean = {
               inUseItrIndex = (1 + inUseItrIndex) % iterators.size
 
               if (stopIdx != inUseItrIndex) {
-                doesHaveNext(inUseItrIndex) || r(stopIdx)
+                unsafeHasNext(inUseItrIndex) || r(stopIdx)
               } else false
             }
 
             r(inUseItrIndex)
           }
-
-          doesHaveNext(inUseItrIndex) || doOthersHaveNext
         } else false
       }
 
