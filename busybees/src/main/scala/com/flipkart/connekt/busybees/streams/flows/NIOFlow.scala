2a1103812f3595aba40d1662576d0e31b68f971a
Kinshuk Bairagi <me@kinshuk.in>
2016-03-12 21:21:56 +0530

    NIO flow change to def

--- busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/NIOFlow.scala
+++ busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/NIOFlow.scala
@@ -14,7 +14,7 @@ abstract class NIOFlow[In, Out](parallelism: Int)(ec: ExecutionContextExecutor) 
 
   def map: In => List[Out]
 
-  val flow = Flow[In].map(identity).mapAsync(parallelism){
+  def flow = Flow[In].map(identity).mapAsync(parallelism){
     i => Future(map(i))(ec)
   }.mapConcat(identity)
 }
