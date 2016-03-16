e0fd34bf05c6088481f808858b8a073a8467c50c
aman.shrivastava <aman.shrivastava@flipkart.com>
2016-03-16 18:09:08 +0530

    CNKT-00: GCM write service, buffer from config

--- busybees/src/main/scala/com/flipkart/connekt/busybees/streams/ConnektTopology.scala
+++ busybees/src/main/scala/com/flipkart/connekt/busybees/streams/ConnektTopology.scala
@@ -18,7 +18,7 @@ trait ConnektTopology[E <:CallbackEvent] {
   def sink: Sink[E, NotUsed]
 
   def graph() = source.withAttributes(ActorAttributes.dispatcher("akka.actor.default-pinned-dispatcher")).via(transform).to(sink)
-    .withAttributes(Attributes.inputBuffer(initial = 16, max = 64))
+//    .withAttributes(Attributes.inputBuffer(initial = 16, max = 64))
     .withAttributes(Attributes.logLevels(onElement = Logging.InfoLevel, onFinish = Logging.InfoLevel, onFailure = Logging.ErrorLevel))
 
   def run(implicit mat: Materializer) = graph().run()
