a1df0ae3a94fb133f6e03f68c7f243de33f76345
aman.shrivastava <aman.shrivastava@flipkart.com>
2016-03-23 17:00:00 +0530

    CNKT-00: ignoring stencil bench mark test

--- busybees/src/test/scala/com/flipkart/connekt/busybees/tests/streams/topologies/StencilBenchmarkTest.scala
+++ busybees/src/test/scala/com/flipkart/connekt/busybees/tests/streams/topologies/StencilBenchmarkTest.scala
@@ -12,6 +12,7 @@ import com.flipkart.connekt.commons.iomodels.ConnektRequest
 import com.flipkart.connekt.commons.metrics.Instrumented
 import com.flipkart.connekt.commons.services.ConnektConfig
 import com.flipkart.connekt.commons.utils.StringUtils._
+import jdk.nashorn.internal.ir.annotations.Ignore
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -19,6 +20,7 @@ import scala.concurrent.duration._
 /**
  * @author aman.shrivastava on 16/03/16.
  */
+@Ignore
 class StencilBenchmarkTest extends TopologyUTSpec with Instrumented {
 
   override def beforeAll() = {
@@ -29,55 +31,9 @@ class StencilBenchmarkTest extends TopologyUTSpec with Instrumented {
   val counter: AtomicLong = new AtomicLong(0)
   val counterTS: AtomicLong = new AtomicLong(System.currentTimeMillis)
 
-  "StencilBenchmarkTest" should "log stencil rendering rates for a vanilla graph Velocity" in {
+  lazy val ioDispatcher = system.dispatchers.lookup("akka.actor.io-dispatcher")
+  lazy val fmtAndroidParallelism = ConnektConfig.getInt("topology.push.androidFormatter.parallelism").get
 
-    val topic = ServiceFactory.getPNMessageService.getTopicNames(Channel.PUSH).get.head
-    //    val kSource = new KafkaSource[ConnektRequest](getKafkaConsumerHelper, topic)(Promise[String]().future)
-    val qps = meter("android.send")
-
-    val repeatSource = Source.repeat {
-      """
-        |{
-        |	"channel": "PN",
-        |	"sla": "H",
-        | "templateId": "cktSampleApp-stn0VEL",
-        | "channelDataModel": {
-        |     "message" : "hello",
-        |     "id" : "abcd",
-        |     "triggerSound" : false,
-        |     "notificationType" : "text",
-        |     "title" : "velocity stencil"
-        | },
-        |	"channelInfo" : {
-        |	    "type" : "PN",
-        |	    "ackRequired": true,
-        |    	"delayWhileIdle": true,
-        |     "platform" :  "android",
-        |     "appName" : "ConnectSampleApp",
-        |     "deviceId" : ["513803e45cf1b344ef494a04c9fb650a"]
-        |	},
-        |	"meta": {
-        |   "x-perf-test" : "true"
-        | }
-        |}
-      """.stripMargin.getObj[ConnektRequest]
-    }
-
-    val renderFlow = Flow.fromGraph(new RenderFlow).map(rT => {
-      qps.mark()
-      if(0 == (counter.incrementAndGet() % 1000)) {
-        val now = System.currentTimeMillis
-        val rate = 1000*1000/(now - counterTS.getAndSet(now))
-        println(s"Processed ${counter.get()} messages by $now, RATE = $rate,  MR[${qps.getMeanRate}}], 1MR[${qps.getOneMinuteRate}}]")
-      }
-    })
-
-
-    //Run the benchmark topology
-    val rF = Source.fromGraph(repeatSource).via(renderFlow).runWith(Sink.ignore)
-
-    Await.result(rF, 180.seconds)
-  }
 
   "StencilBenchmarkTest" should "log stencil rendering rates for a vanilla graph Groovy" in {
 
@@ -90,7 +46,7 @@ class StencilBenchmarkTest extends TopologyUTSpec with Instrumented {
         |{
         |	"channel": "PN",
         |	"sla": "H",
-        | "templateId": "cktSampleApp-stn0x1",
+        | "templateId": "ckt-retailqa-android",
         | "channelDataModel": {
         |     "message" : "hello",
         |     "id" : "abcd",
@@ -121,6 +77,7 @@ class StencilBenchmarkTest extends TopologyUTSpec with Instrumented {
         println(s"Processed ${counter.get()} messages by $now, RATE = $rate,  MR[${qps.getMeanRate}}], 1MR[${qps.getOneMinuteRate}}]")
       }
     })
+
 
 
     //Run the benchmark topology
