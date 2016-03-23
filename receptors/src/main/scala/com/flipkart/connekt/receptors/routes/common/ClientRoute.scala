bd2c0e3e043f2e492cdf5370366027bb7e9afd8b
Durga Prasana <durga.s@flipkart.com>
2016-03-24 00:50:53 +0530

    add admin api(s) to swagger

--- receptors/src/main/scala/com/flipkart/connekt/receptors/routes/common/ClientRoute.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/routes/common/ClientRoute.scala
@@ -14,13 +14,11 @@ package com.flipkart.connekt.receptors.routes.common
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.stream.ActorMaterializer
-import com.flipkart.connekt.commons.entities.Channel.Channel
 import com.flipkart.connekt.commons.entities.{AppUser, AppUserConfiguration, Channel, UserType}
 import com.flipkart.connekt.commons.factories.ServiceFactory
 import com.flipkart.connekt.commons.iomodels.{GenericResponse, Response}
 import com.flipkart.connekt.commons.services.UserConfigurationService
 import com.flipkart.connekt.commons.sync.{SyncManager, SyncMessage, SyncType}
-import com.flipkart.connekt.receptors.directives.ChannelSegment
 import com.flipkart.connekt.receptors.routes.BaseJsonHandler
 
 class ClientRoute(implicit am: ActorMaterializer, user: AppUser) extends BaseJsonHandler {
@@ -42,16 +40,6 @@ class ClientRoute(implicit am: ActorMaterializer, user: AppUser) extends BaseJso
           get {
             val data = Channel.values.flatMap(ch => UserConfigurationService.get(clientName, ch).get)
             complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"Fetched ClientConfig for client: $clientName", data)))
-          }
-      } ~ path(Segment / "config" / ChannelSegment) {
-        (clientName: String, channel: Channel) =>
-          get {
-            UserConfigurationService.get(clientName, channel).get match {
-              case Some(data) =>
-                complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"Fetched ClientConfig ", data)))
-              case None =>
-                complete(GenericResponse(StatusCodes.NotFound.intValue, null, Response(s"Fetching ClientDetails failed.", Map("clientName" -> clientName, "channel" -> channel))))
-            }
           }
       } ~ pathPrefix("create") {
         pathEndOrSingleSlash {
