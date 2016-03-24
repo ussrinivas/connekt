3ce49879593f341d78592b5e491e8df33243a4a3
Kinshuk Bairagi <me@kinshuk.in>
2016-03-24 16:11:22 +0530

    Fix swagger fetch devices for user api

--- receptors/src/main/scala/com/flipkart/connekt/receptors/routes/push/RegistrationRoute.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/routes/push/RegistrationRoute.scala
@@ -29,69 +29,69 @@ import scala.util.Failure
 class RegistrationRoute(implicit am: ActorMaterializer, user: AppUser) extends BaseJsonHandler {
 
   val register = pathPrefix("v1") {
-        pathPrefix("registration" / "push") {
-        path(MPlatformSegment / Segment / Segment) {
-          (platform: MobilePlatform, appName: String, deviceId: String) =>
-            put {
-              authorize(user, "REGISTRATION", s"REGISTRATION_$appName") {
-                entity(as[DeviceDetails]) { d =>
-                  val newDeviceDetails = d.copy(appName = appName, osName = platform.toString, deviceId = deviceId, active = true)
+    pathPrefix("registration" / "push") {
+      path(MPlatformSegment / Segment / Segment) {
+        (platform: MobilePlatform, appName: String, deviceId: String) =>
+          put {
+            authorize(user, "REGISTRATION", s"REGISTRATION_$appName") {
+              entity(as[DeviceDetails]) { d =>
+                val newDeviceDetails = d.copy(appName = appName, osName = platform.toString, deviceId = deviceId, active = true)
 
-                  val result = DeviceDetailsService.get(appName, deviceId).transform[Either[Unit, Unit]]({
+                val result = DeviceDetailsService.get(appName, deviceId).transform[Either[Unit, Unit]]({
                     case Some(deviceDetail) => DeviceDetailsService.update(deviceId, newDeviceDetails).map(u => Left(Unit))
                     case None => DeviceDetailsService.add(newDeviceDetails).map(c => Right(Unit))
                   }, Failure(_)).get
 
-                  result match {
-                    case Right(x) =>
-                      complete(GenericResponse(StatusCodes.Created.intValue, null, Response(s"DeviceDetails created for ${newDeviceDetails.deviceId}", newDeviceDetails)))
-                    case Left(x) =>
-                      complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"DeviceDetails updated for ${newDeviceDetails.deviceId}", newDeviceDetails)))
-                  }
-                }
-              }
-            } ~ delete {
-              authorize(user, "REGISTRATION", s"REGISTRATION_$appName") {
-                DeviceDetailsService.delete(appName, deviceId).get
-                complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"DeviceDetails deleted for $deviceId", null)))
-              }
-            }
-        } ~ path(Segment / "users" / Segment) {
-          (appName: String, userId: String) =>
-            get {
-              authorize(user, "REGISTRATION_READ", s"REGISTRATION_READ_$appName") {
-                val deviceDetails = DeviceDetailsService.getByUserId(appName, userId).get
-                complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"DeviceDetails fetched for app: $appName, user: $userId", Map[String, Any]("deviceDetails" -> deviceDetails))))
-              }
-            }
-        } ~ path(MPlatformSegment / Segment / Segment) {
-          (platform: MobilePlatform, appName: String, deviceId: String) =>
-            get {
-              authorize(user, "REGISTRATION_READ", s"REGISTRATION_READ_$appName") {
-                DeviceDetailsService.get(appName, deviceId).get match {
-                  case Some(deviceDetail) =>
-                    complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"DeviceDetails fetched for app: $appName id: $deviceId", Map[String, Any]("deviceDetails" -> deviceDetail))))
-                  case None =>
-                    complete(GenericResponse(StatusCodes.NotFound.intValue, null, Response(s"No DeviceDetails found for app: $appName id: $deviceId", null)))
+                result match {
+                  case Right(x) =>
+                    complete(GenericResponse(StatusCodes.Created.intValue, null, Response(s"DeviceDetails created for ${newDeviceDetails.deviceId}", newDeviceDetails)))
+                  case Left(x) =>
+                    complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"DeviceDetails updated for ${newDeviceDetails.deviceId}", newDeviceDetails)))
                 }
               }
             }
-        } ~ path(Segment / "snapshot") {
-          (appName: String) =>
-            get {
-              authorize(user, "REGISTRATION_DOWNLOAD", s"REGISTRATION_DOWNLOAD_$appName") {
-                ConnektLogger(LogFile.SERVICE).info(s"REGISTRATION_DOWNLOAD for $appName started by ${user.userId}")
-                val dataStream = DeviceDetailsService.getAll(appName).get
-                def chunks = Source.fromIterator(() => dataStream)
-                  .grouped(100)
-                  .map(d => d.map(_.getJson).mkString(scala.compat.Platform.EOL))
-                  .map(HttpEntity.ChunkStreamPart.apply)
-                val response = HttpResponse(entity = HttpEntity.Chunked(MediaTypes.`application/json`, chunks))
-                complete(response)
+          } ~ delete {
+            authorize(user, "REGISTRATION", s"REGISTRATION_$appName") {
+              DeviceDetailsService.delete(appName, deviceId).get
+              complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"DeviceDetails deleted for $deviceId", null)))
+            }
+          }
+      } ~ path(Segment / "users" / Segment) {
+        (appName: String, userId: String) =>
+          get {
+            authorize(user, "REGISTRATION_READ", s"REGISTRATION_READ_$appName") {
+              val deviceDetails = DeviceDetailsService.getByUserId(appName, userId).get
+              complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"DeviceDetails fetched for app: $appName, user: $userId", Map[String, Any]("deviceDetails" -> deviceDetails))))
+            }
+          }
+      } ~ path(MPlatformSegment / Segment / Segment) {
+        (platform: MobilePlatform, appName: String, deviceId: String) =>
+          get {
+            authorize(user, "REGISTRATION_READ", s"REGISTRATION_READ_$appName") {
+              DeviceDetailsService.get(appName, deviceId).get match {
+                case Some(deviceDetail) =>
+                  complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"DeviceDetails fetched for app: $appName id: $deviceId", Map[String, Any]("deviceDetails" -> deviceDetail))))
+                case None =>
+                  complete(GenericResponse(StatusCodes.NotFound.intValue, null, Response(s"No DeviceDetails found for app: $appName id: $deviceId", null)))
               }
             }
-        }
-
+          }
+      } ~ path(Segment / "snapshot") {
+        (appName: String) =>
+          get {
+            authorize(user, "REGISTRATION_DOWNLOAD", s"REGISTRATION_DOWNLOAD_$appName") {
+              ConnektLogger(LogFile.SERVICE).info(s"REGISTRATION_DOWNLOAD for $appName started by ${user.userId}")
+              val dataStream = DeviceDetailsService.getAll(appName).get
+              def chunks = Source.fromIterator(() => dataStream)
+                .grouped(100)
+                .map(d => d.map(_.getJson).mkString(scala.compat.Platform.EOL))
+                .map(HttpEntity.ChunkStreamPart.apply)
+              val response = HttpResponse(entity = HttpEntity.Chunked(MediaTypes.`application/json`, chunks))
+              complete(response)
+            }
+          }
       }
+
     }
+  }
 }
