6c335ac6c92abcae1426858b74bc681de5940be2
Kinshuk Bairagi <me@kinshuk.in>
2016-04-05 17:33:41 +0530

    GCM Request/Tracket move to Seq in order to preserve ordering of deviceIds

--- busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/formaters/AndroidChannelFormatter.scala
+++ busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/formaters/AndroidChannelFormatter.scala
@@ -19,10 +19,10 @@ import com.flipkart.connekt.busybees.streams.flows.NIOFlow
 import com.flipkart.connekt.commons.entities.MobilePlatform
 import com.flipkart.connekt.commons.factories.{ConnektLogger, LogFile}
 import com.flipkart.connekt.commons.helpers.CallbackRecorder._
-import com.flipkart.connekt.commons.helpers.ConnektRequestHelper._
 import com.flipkart.connekt.commons.iomodels._
 import com.flipkart.connekt.commons.services.{DeviceDetailsService, PNStencilService, StencilService}
 import com.flipkart.connekt.commons.utils.StringUtils._
+import com.flipkart.connekt.commons.helpers.ConnektRequestHelper._
 
 import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration._
@@ -37,9 +37,9 @@ class AndroidChannelFormatter(parallelism: Int)(implicit ec: ExecutionContextExe
 
       val pnInfo = message.channelInfo.asInstanceOf[PNRequestInfo]
 
-      val devicesInfo = DeviceDetailsService.get(pnInfo.appName, pnInfo.deviceIds).get
-      val validDeviceIds = devicesInfo.map(_.deviceId).toSet
-      val invalidDeviceIds = pnInfo.deviceIds.diff(validDeviceIds)
+      val devicesInfo = DeviceDetailsService.get(pnInfo.appName, pnInfo.deviceIds).get.toSeq
+      val validDeviceIds = devicesInfo.map(_.deviceId)
+      val invalidDeviceIds = pnInfo.deviceIds.diff(validDeviceIds.toSet)
       invalidDeviceIds.map(PNCallbackEvent(message.id, _, InternalStatus.MissingDeviceInfo, MobilePlatform.ANDROID, pnInfo.appName, message.contextId.orEmpty)).persist
 
       val tokens = devicesInfo.map(_.token)
@@ -53,7 +53,7 @@ class AndroidChannelFormatter(parallelism: Int)(implicit ec: ExecutionContextExe
         tokens.map(t => pnInfo.platform.toUpperCase match {
           case "ANDROID" => GCMPNPayload(registration_ids = tokens, delay_while_idle = Option(pnInfo.delayWhileIdle), appDataWithId, time_to_live = Some(ttl), dry_run = dryRun)
           case "OPENWEB" => OpenWebGCMPayload(registration_ids = tokens, dry_run = None)
-        }).map(GCMPayloadEnvelope(message.id, validDeviceIds, pnInfo.appName, message.contextId.orEmpty, _))
+        }).map(GCMPayloadEnvelope(message.id,validDeviceIds, pnInfo.appName, message.contextId.orEmpty , _)).toList
       } else {
         ConnektLogger(LogFile.PROCESSORS).warn(s"AndroidChannelFormatter dropping ttl-expired message: ${message.id}")
         devicesInfo.map(d => PNCallbackEvent(message.id, d.deviceId, InternalStatus.TTLExpired, MobilePlatform.ANDROID, d.appName, message.contextId.orEmpty)).persist
--- commons/src/main/scala/com/flipkart/connekt/commons/iomodels/GCMPayload.scala
+++ commons/src/main/scala/com/flipkart/connekt/commons/iomodels/GCMPayload.scala
@@ -15,15 +15,15 @@ package com.flipkart.connekt.commons.iomodels
 import com.fasterxml.jackson.annotation.JsonInclude
 
 
-case class GCMPayloadEnvelope(messageId: String, deviceId: Set[String], appName: String, contextId:String, gcmPayload: GCMPayload)
+case class GCMPayloadEnvelope(messageId: String, deviceId: Seq[String], appName: String, contextId:String, gcmPayload: GCMPayload)
 
 abstract class GCMPayload
 
-case class GCMPNPayload(registration_ids: List[String],
+case class GCMPNPayload(registration_ids: Seq[String],
                         @JsonInclude(JsonInclude.Include.NON_NULL) delay_while_idle: Option[Boolean],
                         data: Any,
                         @JsonInclude(JsonInclude.Include.NON_NULL) time_to_live: Option[Long] = None,
                         @JsonInclude(JsonInclude.Include.NON_NULL) dry_run: Option[Boolean] = None) extends GCMPayload
 
-case class OpenWebGCMPayload(registration_ids: List[String],
+case class OpenWebGCMPayload(registration_ids: Seq[String],
                              @JsonInclude(JsonInclude.Include.NON_NULL) dry_run: Option[Boolean] = None) extends GCMPayload
