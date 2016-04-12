6ec3702580b4f53ccdbe1e78c14876402e17b661
amanshrivastava <aman.shrivastava@flipkart.com>
2016-04-12 17:45:08 +0530

    metrics for response events (#88)

--- commons/src/main/scala/com/flipkart/connekt/commons/helpers/CallbackRecorder.scala
+++ commons/src/main/scala/com/flipkart/connekt/commons/helpers/CallbackRecorder.scala
@@ -16,9 +16,10 @@ import com.flipkart.connekt.commons.core.Wrappers._
 import com.flipkart.connekt.commons.entities.Channel
 import com.flipkart.connekt.commons.factories.ServiceFactory
 import com.flipkart.connekt.commons.iomodels.PNCallbackEvent
+import com.flipkart.connekt.commons.metrics.Instrumented
 import com.flipkart.connekt.commons.services.BigfootService
 
-object CallbackRecorder {
+object CallbackRecorder extends Instrumented {
 
   implicit def callbackRecorder(event: PNCallbackEvent): PNListCallbackRecorder = new PNListCallbackRecorder(List(event))
 
@@ -26,6 +27,7 @@ object CallbackRecorder {
     def persist = Try_ {
       events.foreach(e => {
         ServiceFactory.getCallbackService.persistCallbackEvent(e.messageId, s"${e.appName.toLowerCase}${e.deviceId}", Channel.PUSH, e)
+        meter(s"event.${e.eventType}").mark()
         BigfootService.ingest(e.toBigfootFormat)
       })
     }
