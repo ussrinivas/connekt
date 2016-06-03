0b0cf146bd82c00cb8297448d639825bc57c4070
Kinshuk Bairagi <me@kinshuk.in>
2016-06-03 16:39:19 +0530

    Fix /ReportingService TestCases

--- commons/src/test/scala/com/flipkart/connekt/commons/tests/services/ReportingServiceTest.scala
+++ commons/src/test/scala/com/flipkart/connekt/commons/tests/services/ReportingServiceTest.scala
@@ -16,13 +16,13 @@ class ReportingServiceTest extends CommonsBaseTest {
   }
 
   "ReportingService" should "adds delta count " in {
-    noException should be thrownBy ServiceFactory.getReportingService.recordPushStatsDelta("clientId", None, None, None, null, null, 1)
+    noException should be thrownBy ServiceFactory.getReportingService.recordPushStatsDelta("clientId", None, None, None, "app_name", "random_event", 1)
   }
 
   "ReportingService" should "get single report details " in {
     ServiceFactory.getReportingService.recordPushStatsDelta("clientId-test", None, None, None, "APP_NAME", null, 10)
     ServiceFactory.getReportingService.statsUpdateTask.run()
-    ServiceFactory.getReportingService.getAllDetails(date, "clientId-test", None, None, None, None) shouldEqual Map("push.APP_NAME" -> 10)
+    ServiceFactory.getReportingService.getAllDetails(date, "clientId-test", None, None, None, None) shouldEqual Map("push.app_name" -> 10)
   }
 
 }
