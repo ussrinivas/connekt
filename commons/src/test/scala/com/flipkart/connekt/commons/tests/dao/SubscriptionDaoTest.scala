0a3657266f67f3ba1be1ff7af17de47d0b9579e6
HARSHIT SINHA <harshit.sinha@Harshit-Sinha.local>
2016-06-19 15:10:49 +0530

    updated the SubscriptionDaoTest.scala for SubscriptionDao.scala
    Signed-off-by: HARSHIT SINHA <harshit.sinha@Harshit-Sinha.local>

---
+++ commons/src/test/scala/com/flipkart/connekt/commons/tests/dao/SubscriptionDaoTest.scala
@@ -0,0 +1,36 @@
+package com.flipkart.connekt.commons.tests.dao
+
+import java.util.UUID
+
+import com.flipkart.connekt.commons.entities._
+import com.flipkart.connekt.commons.dao.DaoFactory
+import com.flipkart.connekt.commons.tests.CommonsBaseTest
+
+/**
+  * Created by harshit.sinha on 17/06/16.
+  */
+
+class SubscriptionDaoTest extends CommonsBaseTest {
+
+  val subscription = new Subscription()
+
+  subscription.name = "SubscriptionDaoTest"
+  subscription.id = UUID.randomUUID().toString
+  subscription.shutdownThreshold = 4
+  subscription.createdBy = "connekt-insomnia"
+  subscription.relayPoint = new HTTPRelayPoint("http://localhost:8080/testingRoute")
+  subscription.groovyFilter = "This is a groovyFilter string for testing"
+
+  "add test" should "return true" in {
+    assert(DaoFactory.getSubscriptionDao.add(subscription))
+  }
+
+  "get test" should "return a Subscription" in {
+    assert(DaoFactory.getSubscriptionDao.get(subscription.id).isDefined)
+  }
+
+  "delete test" should "return true" in {
+    assert(DaoFactory.getSubscriptionDao.delete(subscription.id))
+  }
+
+}
