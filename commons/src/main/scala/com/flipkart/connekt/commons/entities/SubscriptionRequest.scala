59e5d97491bbe561c9c006b6e120c6386a870977
HARSHIT SINHA <harshit.sinha@Harshit-Sinha.local>
2016-06-19 15:15:51 +0530

    removing redundant class
    Signed-off-by: HARSHIT SINHA <harshit.sinha@Harshit-Sinha.local>

--- commons/src/main/scala/com/flipkart/connekt/commons/entities/SubscriptionRequest.scala
+++
@@ -1,6 +0,0 @@
-package com.flipkart.connekt.commons.entities
-
-/**
-  * Created by harshit.sinha on 10/06/16.
-  */
-case class SubscriptionRequest(sName: String, endpoint: Endpoint, groovyString: String, shutdownThreshold: Int/*, contact: String, failureThreshold: Int*/)
