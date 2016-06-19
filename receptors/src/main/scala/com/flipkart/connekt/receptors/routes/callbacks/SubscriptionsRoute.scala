0bb0d416343063dfd6ecbdc8113fe41ab9d6fb8d
HARSHIT SINHA <harshit.sinha@Harshit-Sinha.local>
2016-06-20 00:40:02 +0530

    updated routes as per the SubscriptionServices
    Signed-off-by: HARSHIT SINHA <harshit.sinha@Harshit-Sinha.local>

--- receptors/src/main/scala/com/flipkart/connekt/receptors/routes/callbacks/SubscriptionsRoute.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/routes/callbacks/SubscriptionsRoute.scala
@@ -2,7 +2,7 @@ package com.flipkart.connekt.receptors.routes.callbacks
 
 import akka.http.scaladsl.model.StatusCodes
 import akka.stream.ActorMaterializer
-import com.flipkart.connekt.commons.entities.SubscriptionRequest
+import com.flipkart.connekt.commons.entities.Subscription
 import com.flipkart.connekt.commons.iomodels.{GenericResponse, Response}
 import com.flipkart.connekt.receptors.routes.BaseJsonHandler
 import com.flipkart.connekt.receptors.wire.ResponseUtils._
@@ -11,81 +11,62 @@ import com.flipkart.connekt.commons.sync.{SyncManager, SyncMessage, SyncType}
 
 import scala.util.{Failure, Success}
 
-case class ActionRequested(action: String)
+/**
+  * Created by harshit.sinha on 07/06/16.
+  */
 class SubscriptionsRoute(implicit am: ActorMaterializer) extends BaseJsonHandler {
 
   val route = pathPrefix("v1" / "subscription") {
     authenticate { user =>
       pathEndOrSingleSlash {
         post {
-          entity(as[SubscriptionRequest]) { e =>
-            SubscriptionService.writeSubscription(e, user.userId) match {
-              case Success(optionSubscription) =>
-                optionSubscription match {
-                  case Some(subscription) =>
-                    complete(GenericResponse(StatusCodes.OK.intValue, null, Response("subscription created", subscription)))
-                  case None => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("subscription failed", null)))
-                }
-              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("subscription failed", null)))
+          entity(as[Subscription]) { sub =>
+            sub.createdBy = user.userId
+            SubscriptionService.add(sub) match {
+              case Success(createdSubscription) => complete(GenericResponse(StatusCodes.OK.intValue, null, Response("Subscription created", createdSubscription)))
+              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("Subscription creation failed: " + e, null)))
             }
           }
         }
       } ~
-      pathPrefix(Segment) { (sId: String) =>
+      pathPrefix(Segment) { (id: String) =>
         post {
-          entity(as[SubscriptionRequest]) { e =>
-            SubscriptionService.updateSubscription(e, sId, user.userId) match {
-              case Success(optionSubscription) =>
-                optionSubscription match {
-                  case Some(subscription) =>
-                    complete(GenericResponse(StatusCodes.OK.intValue, null, Response("updating successful", subscription)))
-                  case None => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("updating failed", null)))
-                }
-              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("updating failed", null)))
+          entity(as[Subscription]) { sub =>
+            sub.createdBy = user.userId
+            SubscriptionService.update(sub, id) match {
+              case Success(updatedSubscription) => complete(GenericResponse(StatusCodes.OK.intValue, null, Response("Subscription updated", updatedSubscription)))
+              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("Subscription updation failed: "+ e, null)))
             }
           }
         } ~
         get {
           pathEndOrSingleSlash {
-            SubscriptionService.getSubscription(sId, user.userId) match {
-              case Success(optionSubscription) =>
-                optionSubscription match {
-                  case Some(subscription) => complete(GenericResponse(StatusCodes.OK.intValue, null, Response("fetch successful", subscription)))
-                  case None => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("fetch failed", null)))
-                }
-              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("fetch failed", null)))
+            SubscriptionService.get(id) match {
+              case Success(fetchedSubscription) => complete(GenericResponse(StatusCodes.OK.intValue, null, Response("Subscription fetched", fetchedSubscription)))
+              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("Subscription fetching failed: "+ e, null)))
             }
           } ~
           path("start") {
-            SubscriptionService.getSubscription(sId, user.userId) match {
-              case Success(optionSubscription) =>
-                optionSubscription match {
-                  case Some(subscription) =>
-                    SyncManager.get.publish(SyncMessage(topic = SyncType.SUBSCRIPTION_REQUEST, List("start", subscription)))
-                    complete(GenericResponse(StatusCodes.OK.intValue, null, Response("start successful", subscription)))
-                  case None => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("start failed", null)))
-                }
-              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("start failed", null)))
+            SubscriptionService.get(id) match {
+              case Success(subscription) =>
+                SyncManager.get().publish(SyncMessage(topic = SyncType.SUBSCRIPTION_REQUEST, List("start", subscription)))
+                complete(GenericResponse(StatusCodes.OK.intValue, null, Response("Subscription started successfully", subscription)))
+              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("Subscription starting failed: "+ e, null)))
             }
           } ~
           path("stop") {
-            SubscriptionService.getSubscription(sId, user.userId) match {
-              case Success(optionSubscription) =>
-                optionSubscription match {
-                  case Some(subscription) =>
-                    SyncManager.get.publish(SyncMessage(topic = SyncType.SUBSCRIPTION_REQUEST, List("stop", subscription)))
-                    complete(GenericResponse(StatusCodes.OK.intValue, null, Response("stop successful", subscription)))
-                  case None => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("stop failed", null)))
-                }
-              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("stop failed", null)))
+            SubscriptionService.get(id) match {
+              case Success(subscription) =>
+                SyncManager.get().publish(SyncMessage(topic = SyncType.SUBSCRIPTION_REQUEST, List("stop", subscription)))
+                complete(GenericResponse(StatusCodes.OK.intValue, null, Response("Subscription stopped successfully", subscription)))
+              case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("Subscription stopping failed: "+ e, null)))
             }
           }
         } ~
         delete {
-          SubscriptionService.deleteSubscription(sId, user.userId) match {
-            case Success(code) =>
-              complete(GenericResponse(StatusCodes.OK.intValue, null, Response("deletion successful", null)))
-            case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("deletion failed", null)))
+          SubscriptionService.remove(id) match {
+            case Success(code) => complete(GenericResponse(StatusCodes.OK.intValue, null, Response("Subscription deleted successfully", null)))
+            case Failure(e) => complete(GenericResponse(StatusCodes.BadRequest.intValue, null, Response("Subsciption deletion failed: "+ e, null)))
           }
         }
       }
--- receptors/src/test/scala/com/flipkart/connekt/receptors/tests/routes/callbacks/SubscriptionsRouteTest.scala
+++ receptors/src/test/scala/com/flipkart/connekt/receptors/tests/routes/callbacks/SubscriptionsRouteTest.scala
@@ -1,40 +1,118 @@
 package com.flipkart.connekt.receptors.tests.routes.callbacks
 
-import akka.http.scaladsl.model.{HttpEntity, MediaTypes, StatusCodes}
+import akka.http.scaladsl.model.{HttpEntity, HttpResponse, MediaTypes, StatusCodes}
 import akka.http.scaladsl.model.headers.RawHeader
-import com.flipkart.connekt.receptors.routes.callbacks.{CallbackRoute, SubscriptionsRoute}
+import com.flipkart.connekt.commons.entities.Subscription
+import com.flipkart.connekt.receptors.routes.callbacks.SubscriptionsRoute
 import com.flipkart.connekt.receptors.tests.routes.BaseRouteTest
+import org.apache.commons.lang.StringEscapeUtils
+import com.flipkart.connekt.commons.utils.StringUtils._
 
 /**
   * Created by harshit.sinha on 08/06/16.
   */
+
 class SubscriptionsRouteTest extends BaseRouteTest {
 
-  val userId = "connekt-insomnia"
-  val headerTest = RawHeader("x-api-key", "e    ")
+  val subscriptionRoute = new SubscriptionsRoute().route
+  override val header = RawHeader("x-api-key", "b0979afd-2ce3-4786-af62-ab53f88204ae")
+  var subscription: Subscription = _
 
-  "subscriptionsRoute Test " should "return Ok" in {
-    val subscriptionRoute = new SubscriptionsRoute().route
-    val payload =
+  "Create Test" should "return OK" in {
+    val groovyString =
+      """
+        |package com.flipkart.connekt.commons.entities;
+        |import com.flipkart.connekt.commons.iomodels.CallbackEvent
+        |import com.flipkart.connekt.commons.iomodels.PNCallbackEvent;
+        |class ConnektSampleAppGroovy implements Evaluator {
+        |public boolean evaluate(CallbackEvent context) {
+        |return (context as PNCallbackEvent).eventType().equals("gcm_received")
+        |}
+        |
+        |}
+      """.stripMargin
+
+    val JSONRequest =
       s"""
          |{
-         |	"event": "PN",
-         |	"eventType": "received",
-         |	"timestamp": 1449597968,
-         |	"contextId": "connekt-wx-01"
+         |	"sName" : "SubscriptionRouteTest",
+         |	"endpoint":
+         |    {
+         |        "type" : "HTTP",
+         |        "url" : "http://localhost:8080/SubscriptionRouteTesting"
+         |    },
+         |	"groovyString": "${StringEscapeUtils.escapeJava(groovyString)}",
+         |	"shutdownThreshold" : "4"
          |}
       """.stripMargin
 
-    Post(s"/v1/subscription", HttpEntity(MediaTypes.`application/json`, payload)).addHeader(header) ~>
-      subscriptionRoute ~>
-      check {
-        println("client received response: " + response.toString())
-        status shouldEqual StatusCodes.OK
-      }
-
-//    Delete(s"/v1/push/callback/$appName/$deviceId/$mssgId").addHeader(headerTest) ~> callbackRoute ~>
-//      check {
-//        status shouldEqual StatusCodes.OK
-//      }
+    Post("/v1/subscription", HttpEntity(MediaTypes.`application/json`, JSONRequest)).addHeader(header) ~>
+    subscriptionRoute ~> check {
+      subscription = HttpResponse.getJson.getObj
+      status shouldEqual StatusCodes.OK
+    }
   }
+
+  "Get Test" should "return OK" in {
+    Get("/v1/subscription/"+subscription.id).addHeader(header) ~>
+      subscriptionRoute ~> check {
+      status shouldEqual StatusCodes.OK
+    }
+  }
+
+  "Start Test" should "return OK" in {
+    Get("/v1/subscription/"+subscription.id).addHeader(header) ~>
+      subscriptionRoute ~> check {
+      status shouldEqual StatusCodes.OK
+    }
+  }
+
+  "Stop Test" should "return OK" in {
+    Get("/v1/subscription/"+subscription.id).addHeader(header) ~>
+      subscriptionRoute ~> check {
+      status shouldEqual StatusCodes.OK
+    }
+  }
+
+  "Update Test" should "return OK" in {
+    val groovyString =
+      """
+        |ackage com.flipkart.connekt.commons.entities;
+        |import com.flipkart.connekt.commons.iomodels.CallbackEvent
+        |import com.flipkart.connekt.commons.iomodels.PNCallbackEvent;
+        |class ConnektSampleAppGroovy implements Evaluator {
+        |public boolean evaluate(CallbackEvent context) {
+        |return (context as PNCallbackEvent).eventType().equals("gcm_received")
+        |}
+        |
+        |}
+      """.stripMargin
+
+    val payload =
+      s"""
+         |{
+         |	"sName" : "SubscriptionRouteUpdateTest",
+         |	"endpoint":
+         |    {
+         |        "type" : "HTTP",
+         |        "url" : "http://localhost:8080/SubscriptionRouteUpdateTesting"
+         |    },
+         |	"groovyString": "${StringEscapeUtils.escapeJava(groovyString)}",
+         |	"shutdownThreshold" : "2"
+         |}
+      """.stripMargin
+
+    Post("/v1/subscription/"+subscription.id, HttpEntity(MediaTypes.`application/json`, payload)).addHeader(header) ~>
+      subscriptionRoute ~> check {
+      status shouldEqual StatusCodes.OK
+    }
+  }
+
+  "Delete Test" should "return OK" in {
+    Delete("/v1/subscription/"+subscription.id).addHeader(header) ~>
+      subscriptionRoute ~> check {
+      status shouldEqual StatusCodes.OK
+    }
+  }
+
 }
