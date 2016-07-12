01852c12a0ed920c757f0990c1460f2c19caa9de
Subir <subir.dey@flipkart.com>
2016-07-12 14:35:36 +0530

    CNKT-79:change in deserialising upstream

--- busybees/src/main/scala/com/flipkart/connekt/busybees/xmpp/ConnektStanzaListener.scala
+++ busybees/src/main/scala/com/flipkart/connekt/busybees/xmpp/ConnektStanzaListener.scala
@@ -12,30 +12,44 @@
  */
 package com.flipkart.connekt.busybees.xmpp
 
+import java.util.concurrent.ConcurrentLinkedQueue
+
 import akka.actor.ActorRef
 import com.flipkart.connekt.busybees.streams.flows.dispatchers.GcmXmppDispatcher
+import com.flipkart.connekt.commons.factories.{LogFile, ConnektLogger}
 import com.flipkart.connekt.commons.utils.StringUtils._
-import com.flipkart.connekt.commons.iomodels.{XmppUpstreamResponse, XmppReceipt, XmppDownstreamResponse, XmppUpstreamData}
+import com.flipkart.connekt.commons.iomodels._
 import org.jivesoftware.smack.StanzaListener
 import org.jivesoftware.smack.packet.Stanza
+
+import scala.util.{Try, Success, Failure}
 
 /**
  * Created by subir.dey on 22/06/16.
  */
 class ConnektStanzaListener(connectionActor:ActorRef, dispatcher:GcmXmppDispatcher) extends StanzaListener() {
 
-
   override def processPacket(packet: Stanza)  {
     // Extract the GCM message from the packet.
     val packetExtension:GcmXmppPacketExtension = packet.getExtension(XmppConnectionHelper.GCM_NAMESPACE).asInstanceOf[GcmXmppPacketExtension]
-    val jsonGcmDownstreamMessage:XmppDownstreamResponse = packetExtension.json.getObj[XmppDownstreamResponse]
-    if (jsonGcmDownstreamMessage != null)
-        connectionActor ! jsonGcmDownstreamMessage
-    else {
-      var jsonGcmUpstreamMessage:XmppUpstreamResponse = packetExtension.json.getObj[XmppUpstreamResponse]
-      if ( jsonGcmUpstreamMessage == null )
-        jsonGcmUpstreamMessage = packetExtension.json.getObj[XmppUpstreamData]
-      dispatcher.upStreamRecvdCallback.invoke(connectionActor -> packetExtension.json.getObj[XmppUpstreamData])
+    ConnektLogger(LogFile.CLIENTS).debug("Response from GCM:" + packetExtension.json)
+
+    Try (packetExtension.json.getObj[XmppDownstreamResponse]) match {
+      case Success(downStreamMsg:XmppDownstreamResponse) => {
+        ConnektLogger(LogFile.CLIENTS).debug("De Serialised to downstreamobject:" + downStreamMsg)
+        connectionActor ! downStreamMsg
+      }
+      case Failure(thrown) => {
+        Try (packetExtension.json.getObj[XmppUpstreamResponse]) match {
+          case Success(upstream) =>
+            ConnektLogger(LogFile.CLIENTS).debug("De Serialised to upstream:" + upstream)
+            dispatcher.upStreamRecvdCallback.invoke(connectionActor -> upstream)
+          case Failure(ex) =>
+            ConnektLogger(LogFile.CLIENTS).error("Failed to down/upstream:" + packetExtension.json)
+        }
+
+
+      }
     }
   }
 }
