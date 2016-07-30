9aebc285c20a469df49f80233bd709c82ccaa2bc
Kinshuk Bairagi <me@kinshuk.in>
2016-07-30 23:14:01 +0530

    CNKT-001 switch to gocb, refactor and imporve cb cleanup

--- resources/couchbase-cleanup.go
+++ resources/couchbase-cleanup.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-  "os"
+  "reflect"
   "fmt"
   "net/url"
   "io/ioutil"
   "net/http"
   "encoding/json"
+  "gopkg.in/couchbaselabs/gocb.v1"
+  "flag"
 )
 
 func main() {
 
-  hostname :=  "10.33.169.89" // "ce-sandbox-cb-0001.nm.flipkart.com" 
-  arugments := os.Args[1:]
+  hostname := flag.String("host", "localhost", "couchbase cluster host") 
+  size  := flag.Int("size", 1234, "size of chunk")
+  prefix := flag.String("prefix", "", "prefix")
 
-  if(len(arugments) == 0 ){
-  	panic("missing prefix arugments")
-  }
+  flag.Parse()
 
-  prefix := os.Args[1]
+  fmt.Println("Using Prefix:", *prefix, "with Chunksize of", *size)
 
-  if len(prefix) > 0 {
+  if len(*prefix) > 0 {
 
-    resp, err := http.PostForm("http://" + hostname + ":8093/query/service", url.Values{"statement": {"SELECT META(p).id FROM StatsReporting p WHERE META(p).id LIKE '"+prefix+"%'"} })
+    resp, err := http.PostForm("http://" + *hostname + ":8093/query/service", url.Values{"statement": {"SELECT META(p).id FROM StatsReporting p WHERE META(p).id LIKE '" + *prefix + "%'"} })
     if err != nil {
       panic(err)
     }
@@ -38,21 +39,33 @@ func main() {
       panic(err)
     }
 
-    fmt.Println("Response: ", response["results"])
+    fmt.Println("Response Count", len(response["results"].([]interface{})))
 
-    for _, element := range response["results"].([]interface{}) {
-      id := element.(map[string]interface{})["id"].(string)
-      fmt.Println("Deleteing Id... ", id)
+    cluster, err := gocb.Connect("couchbase://" + *hostname + "")
+    if err != nil {
+      panic(err)
+    }
 
-      del, _ := http.PostForm("http://" + hostname + ":8093/query/service", url.Values{"statement": {"DELETE FROM StatsReporting p USE KEYS '" + id + "' RETURNING p"} })
-      defer del.Body.Close()
+    bucket, err := cluster.OpenBucket("StatsReporting", "")
+    if err != nil {
+    	panic(err)
+    }
 
-      bb, err := ioutil.ReadAll(del.Body)
-      if err != nil {
-        panic(err)
+    var chunks = slicer(response["results"].([]interface{}), *size)
+
+    for _, stage := range chunks {
+      var items []gocb.BulkOp
+      for _, element := range stage {
+        id := element.(map[string]interface{})["id"].(string)
+        fmt.Println("Deleteing Id... ", id)
+        items = append(items, &gocb.RemoveOp{Key: id})
       }
 
-      fmt.Println("Response: ", string(bb))
+      err = bucket.Do(items)
+      if err != nil {
+        fmt.Println("Do error:", err)
+        panic(err)
+      }
 
     }
 
@@ -60,3 +73,30 @@ func main() {
     fmt.Println("Prefix Cannot Be empty")
   }
 }
+
+func slicer(a interface{}, b int) [][]interface{} {
+  val := reflect.ValueOf(a)
+
+  origLen := val.Len()
+  outerLen := origLen / b
+  if origLen % b > 0 {
+    outerLen++
+  }
+
+  // Make the output slices with all the correct lengths
+  c := make([][]interface{}, outerLen)
+
+  for i := range c {
+    newLen := b
+    if origLen - (i * b + newLen) < 0 {
+      newLen = origLen % b
+    }
+    c[i] = make([]interface{}, newLen)
+    for j := range c[i] {
+      c[i][j] = val.Index(i * b + j).Interface()
+    }
+  }
+
+  return c
+}
+
