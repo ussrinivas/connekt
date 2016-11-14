a69805678e2f5026aebb475d4cc451a02743d892
Durga Sahoo <durga.s@flipkart.com>
2016-11-14 16:55:16 +0530

    CNKT-0001: [goblack] remove artifactory refs

--- project/AppBuild.scala
+++ project/AppBuild.scala
@@ -34,12 +34,7 @@ object AppBuild extends Build  {
       "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
       "jBoss" at "http://repository.jboss.org/nexus/content/groups/public",
       "Akka Snapshot Repository" at "http://repo.typesafe.com/typesafe/snapshots/",
-      "RoundEights" at "http://maven.spikemark.net/roundeights",
-      "flipkart local-releases" at "http://artifactory.nm.flipkart.com:8081/artifactory/libs-releases-local",
-      "flipkart ext-releases" at "http://artifactory.nm.flipkart.com:8081/artifactory/ext-release-local",
-      "flipkart central" at "http://artifactory.nm.flipkart.com:8081/artifactory/libs-release",
-      "flipkart snapshorts" at "http://artifactory.nm.flipkart.com:8081/artifactory/libs-snapshot",
-      "flipkart clojars" at "http://artifactory.nm.flipkart.com:8081/artifactory/clojars-repo"
+      "RoundEights" at "http://maven.spikemark.net/roundeights"
     ),
     ivyScala := ivyScala.value map {
       _.copy(overrideScalaVersion = true)
