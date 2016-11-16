774436c16bf00f01c999d82c0a84608c89d914c9
Kinshuk Bairagi <me@kinshuk.in>
2016-11-16 20:44:05 +0530

    prepare

--- busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/transformers/EmailProviderPrepare.scala
+++ busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/transformers/EmailProviderPrepare.scala
@@ -12,19 +12,43 @@
  */
 package com.flipkart.connekt.busybees.streams.flows.transformers
 
-import akka.http.scaladsl.model.HttpRequest
+import akka.http.scaladsl.model.{HttpHeader, HttpRequest}
+import akka.http.scaladsl.model.headers.RawHeader
 import com.flipkart.connekt.busybees.models.EmailRequestTracker
 import com.flipkart.connekt.busybees.streams.flows.MapFlowStage
+import com.flipkart.connekt.commons.factories.ServiceFactory
 import com.flipkart.connekt.commons.iomodels.EmailPayloadEnvelope
+import com.flipkart.connekt.commons.services.KeyChainManager
+import com.flipkart.connekt.commons.utils.StringUtils._
 
 
 class EmailProviderPrepare extends MapFlowStage[EmailPayloadEnvelope, (HttpRequest, EmailRequestTracker)] {
+
+  lazy implicit val stencilService = ServiceFactory.getStencilService
+
   override val map: (EmailPayloadEnvelope) => List[(HttpRequest, EmailRequestTracker)] = emailPayloadEnvelope => {
 
     val selectedProvider = emailPayloadEnvelope.provider.last
+    val credentials = KeyChainManager.getSimpleCredential(s"email.${emailPayloadEnvelope.appName}.$selectedProvider").get
 
+    val tracker = EmailRequestTracker(messageId = emailPayloadEnvelope.messageId,
+      clientId = emailPayloadEnvelope.clientId,
+      to = emailPayloadEnvelope.payload.to.map(_.address),
+      cc = emailPayloadEnvelope.payload.cc.map(_.address),
+      appName = emailPayloadEnvelope.appName,
+      contextId = emailPayloadEnvelope.contextId,
+      meta = emailPayloadEnvelope.meta)
 
+    val providerStencil = stencilService.getStencilsByName(s"${emailPayloadEnvelope.appName.toLowerCase}-email-$selectedProvider").find(_.component.equalsIgnoreCase("prepare")).get
 
-    List.empty
+    val result = stencilService.materialize(providerStencil, Map("data" -> emailPayloadEnvelope, "credentials" -> credentials).getJsonNode)
+
+    val httpRequest = result.asInstanceOf[HttpRequest]
+      .addHeader(RawHeader("x-message-id", emailPayloadEnvelope.messageId))
+      .addHeader(RawHeader("x-context-id", emailPayloadEnvelope.contextId))
+
+    println(httpRequest)
+
+    List(Tuple2(httpRequest, tracker))
   }
 }
