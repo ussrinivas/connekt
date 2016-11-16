0a76d1dd4ab01c24e0d0fa7ac3692b8fe21ad10b
Kinshuk Bairagi <me@kinshuk.in>
2016-11-16 17:22:30 +0530

    Added TPublishable for email

--- commons/src/main/scala/com/flipkart/connekt/commons/iomodels/EmailCallbackEvent.scala
+++ commons/src/main/scala/com/flipkart/connekt/commons/iomodels/EmailCallbackEvent.scala
@@ -12,9 +12,8 @@
  */
 package com.flipkart.connekt.commons.iomodels
 
-import com.flipkart.concord.publisher.TPublishRequest
+import com.flipkart.connekt.commons.utils.DateTimeUtils
 import org.apache.commons.lang.RandomStringUtils
-
 
 case class EmailCallbackEvent(messageId: String,
                               clientId: String,
@@ -25,9 +24,13 @@ case class EmailCallbackEvent(messageId: String,
                               cargo: String = null,
                               timestamp: Long = System.currentTimeMillis(),
                               eventId: String = RandomStringUtils.randomAlphabetic(10)) extends CallbackEvent {
-  override def contactId: String = ???
 
-  override def namespace: String = ???
+  override def contactId: String =  s"${appName.toLowerCase}$address"
 
-  override def toPublishFormat: TPublishRequest = ???
+  override def namespace: String = "fkint/mp/connekt/EmailCallbackEvent"
+
+  override def toPublishFormat: fkint.mp.connekt.EmailCallbackEvent = {
+    fkint.mp.connekt.EmailCallbackEvent(messageId = messageId, address = address,eventType = eventType, appName = appName, contextId = contextId, cargo = cargo, timestamp = DateTimeUtils.getStandardFormatted(timestamp))
+  }
+
 }
