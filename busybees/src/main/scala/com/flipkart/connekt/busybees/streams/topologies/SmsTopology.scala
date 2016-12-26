ef683459b6ae00e59f68ef0f6def7b7c23c15a2e
ayush.agarwal <Ayu07672257687>
2016-12-26 19:38:12 +0530

    Minor change

--- busybees/src/main/scala/com/flipkart/connekt/busybees/streams/topologies/SmsTopology.scala
+++ busybees/src/main/scala/com/flipkart/connekt/busybees/streams/topologies/SmsTopology.scala
@@ -81,8 +81,8 @@ class SmsTopology(kafkaConsumerConfig: Config) extends ConnektTopology[SmsCallba
       */
 
     val render = b.add(new RenderFlow().flow)
-    val trackEmailParallelism = ConnektConfig.getInt("topology.email.tracking.parallelism").get
-    val tracking = b.add(new TrackingFlow(trackEmailParallelism).flow)
+    val trackSmsParallelism = ConnektConfig.getInt("topology.sms.tracking.parallelism").get
+    val tracking = b.add(new TrackingFlow(trackSmsParallelism).flow)
     val fmtSMSParallelism = ConnektConfig.getInt("topology.sms.formatter.parallelism").get
     val smsFilter = b.add(Flow[ConnektRequest].filter(_.channelInfo.isInstanceOf[SmsRequestInfo]))
     val fmtSMS = b.add(new SmsChannelFormatter(fmtSMSParallelism)(ioDispatcher).flow)
