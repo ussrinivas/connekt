7e6379f92e813e0b05d5fded653614f63645a632
Kinshuk Bairagi <me@kinshuk.in>
2017-02-17 19:14:47 +0530

    Handle npe

--- commons/src/main/scala/com/flipkart/connekt/commons/iomodels/ConnektRequest.scala
+++ commons/src/main/scala/com/flipkart/connekt/commons/iomodels/ConnektRequest.scala
@@ -60,7 +60,7 @@ case class ConnektRequest(@JsonProperty(required = false) id: String,
           val subject = stencilService.materialize(stencil.filter(_.component.equalsIgnoreCase("subject")).head, channelDataModel).asInstanceOf[String]
           val html = stencilService.materialize(stencil.filter(_.component.equalsIgnoreCase("html")).head, channelDataModel).asInstanceOf[String]
           val txt = stencilService.materialize(stencil.filter(_.component.equalsIgnoreCase("text")).head, channelDataModel).asInstanceOf[String]
-          EmailRequestData(subject = subject, html = html, text = txt, attachments = channelData.asInstanceOf[EmailRequestData].attachments)
+          EmailRequestData(subject = subject, html = html, text = txt, attachments = Option(channelData).map(_.asInstanceOf[EmailRequestData].attachments).orNull)
         case unsupportedChannel =>
           throw new Exception(s"`channelData` compute undefined for $unsupportedChannel")
       }
