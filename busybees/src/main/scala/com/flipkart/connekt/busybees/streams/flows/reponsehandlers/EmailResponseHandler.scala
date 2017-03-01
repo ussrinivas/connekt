6460a6adedb6b94a7c79d5cf94197bd44c82be9c
Ayush <ayush03agarwal@gmail.com>
2017-03-01 14:46:40 +0530

    Logging change (#282)

--- busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/reponsehandlers/EmailResponseHandler.scala
+++ busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/reponsehandlers/EmailResponseHandler.scala
@@ -44,7 +44,7 @@ class EmailResponseHandler(parallelism:Int)(implicit m: Materializer, ec: Execut
     val maybeEmailCallbackEvent = tryResponse match {
       case Success(emailResponse) =>
         meter(s"${requestTracker.provider}.${emailResponse.responseCode}").mark()
-        ConnektLogger(LogFile.PROCESSORS).info(s"EmailResponseHandler received http response for: ${requestTracker.messageId} with provider messageId : ${emailResponse.messageId} / ${emailResponse.responseCode}")
+        ConnektLogger(LogFile.PROCESSORS).info(s"EmailResponseHandler received http response for: ${requestTracker.messageId} with provider messageId : ${emailResponse.messageId} / ${emailResponse.responseCode}, provider: ${requestTracker.provider}")
         emailResponse.responseCode match {
           case s if 2 == (s / 100) =>
             //Good!
@@ -53,22 +53,22 @@ class EmailResponseHandler(parallelism:Int)(implicit m: Materializer, ec: Execut
 
           case f if 4 == (f / 100) =>
             ServiceFactory.getReportingService.recordChannelStatsDelta(clientId = requestTracker.clientId, contextId = Option(requestTracker.contextId), stencilId = requestTracker.meta.get("stencilId").map(_.toString), channel = Channel.EMAIL, appName = requestTracker.appName, event = EmailResponseStatus.AuthError)
-            ConnektLogger(LogFile.PROCESSORS).error(s"EmailResponseHandler http response - auth error for:  ${emailResponse.providerName}/${emailResponse.messageId} code: ${emailResponse.responseCode} response: ${emailResponse.message}")
+            ConnektLogger(LogFile.PROCESSORS).error(s"EmailResponseHandler http response - auth error for:  ${emailResponse.providerName}/${emailResponse.messageId}, code: ${emailResponse.responseCode}, response: ${emailResponse.message}, provider: ${requestTracker.provider}")
             Right((requestTracker.to ++ requestTracker.cc).map(t => EmailCallbackEvent(requestTracker.messageId, requestTracker.clientId, t, EmailResponseStatus.AuthError, requestTracker.appName, requestTracker.contextId, ProviderResponse(emailResponse.messageId, emailResponse.providerName, emailResponse.message).getJson)))
 
           case e if 5 == (e / 100) =>
             ServiceFactory.getReportingService.recordChannelStatsDelta(clientId = requestTracker.clientId, contextId = Option(requestTracker.contextId), stencilId = requestTracker.meta.get("stencilId").map(_.toString), channel = Channel.EMAIL, appName = requestTracker.appName, event = EmailResponseStatus.InternalError)
-            ConnektLogger(LogFile.PROCESSORS).error(s"EmailResponseHandler http response - the server encountered an error while trying to process the request for:  ${emailResponse.providerName}/${emailResponse.messageId}  code: ${emailResponse.responseCode} response: ${emailResponse.message}")
+            ConnektLogger(LogFile.PROCESSORS).error(s"EmailResponseHandler http response - the server encountered an error while trying to process the request for:  ${emailResponse.providerName}/${emailResponse.messageId}, code: ${emailResponse.responseCode}, response: ${emailResponse.message}, provider: ${requestTracker.provider}")
             Left((requestTracker.to ++ requestTracker.cc).map(t => EmailCallbackEvent(requestTracker.messageId, requestTracker.clientId, t, EmailResponseStatus.InternalError, requestTracker.appName, requestTracker.contextId, emailResponse.message)))
           case w =>
             ServiceFactory.getReportingService.recordChannelStatsDelta(clientId = requestTracker.clientId, contextId = Option(requestTracker.contextId), stencilId = requestTracker.meta.get("stencilId").map(_.toString), channel = Channel.EMAIL, appName = requestTracker.appName, event = EmailResponseStatus.Error)
-            ConnektLogger(LogFile.PROCESSORS).error(s"OpenWebResponseHandler http response - response unhandled for: ${emailResponse.providerName}/${emailResponse.messageId}  code: ${emailResponse.responseCode} response: ${emailResponse.message}")
+            ConnektLogger(LogFile.PROCESSORS).error(s"OpenWebResponseHandler http response - response unhandled for: ${emailResponse.providerName}/${emailResponse.messageId},  code: ${emailResponse.responseCode}, response: ${emailResponse.message}, provider: ${requestTracker.provider}")
             Left((requestTracker.to ++ requestTracker.cc).map(t => EmailCallbackEvent(requestTracker.messageId, requestTracker.clientId, t, EmailResponseStatus.Error, requestTracker.appName, requestTracker.contextId, emailResponse.message)))
         }
 
       case Failure(e) =>
         meter(s"${requestTracker.provider}.exception").mark()
-        ConnektLogger(LogFile.PROCESSORS).error(s"EmailResponseHandler failed to send email for: ${requestTracker.messageId} due to: ${e.getClass.getSimpleName}, ${e.getMessage}", e)
+        ConnektLogger(LogFile.PROCESSORS).error(s"EmailResponseHandler failed to send email for: ${requestTracker.messageId} due to: ${e.getClass.getSimpleName} with provider: ${requestTracker.provider}, ${e.getMessage}", e)
         ServiceFactory.getReportingService.recordChannelStatsDelta(clientId = requestTracker.clientId, contextId = Option(requestTracker.contextId), stencilId = requestTracker.meta.get("stencilId").map(_.toString), channel = Channel.EMAIL, appName = requestTracker.appName, event = InternalStatus.ProviderSendError)
         Left((requestTracker.to ++ requestTracker.cc).map(t => EmailCallbackEvent(requestTracker.messageId, requestTracker.clientId, t, InternalStatus.ProviderSendError, requestTracker.appName, requestTracker.contextId, s"EmailResponseHandler-${e.getClass.getSimpleName}-${e.getMessage}")))
     }
