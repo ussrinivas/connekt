1981665014c8fb89c0e7f55b0b1bf686b515f0d0
ayush.agarwal <Ayu07672257687>
2017-03-06 16:20:30 +0530

    Adding channel in report route

--- receptors/src/main/scala/com/flipkart/connekt/receptors/routes/reports/ReportsRoute.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/routes/reports/ReportsRoute.scala
@@ -17,7 +17,7 @@ import akka.stream.ActorMaterializer
 import com.flipkart.connekt.commons.entities.Channel
 import com.flipkart.connekt.commons.entities.Channel._
 import com.flipkart.connekt.commons.factories.ServiceFactory
-import com.flipkart.connekt.commons.iomodels.{GenericResponse, PNCallbackEvent, Response}
+import com.flipkart.connekt.commons.iomodels._
 import com.flipkart.connekt.commons.utils.DateTimeUtils
 import com.flipkart.connekt.receptors.directives.ChannelSegment
 import com.flipkart.connekt.receptors.routes.BaseJsonHandler
@@ -31,29 +31,31 @@ class ReportsRoute(implicit am: ActorMaterializer) extends BaseJsonHandler {
       user =>
         pathPrefix("v1" / "reports") {
           authorize(user, "REPORTS") {
-            pathPrefix("push") {
-              path(Segment / "messages" / Segment / Segment / "events") {
-                (appName: String, deviceId: String, messageId: String) =>
-                  get {
-                    meteredResource(s"reportsPushMessageEvents.$appName") {
-                      val events = ServiceFactory.getCallbackService.fetchCallbackEvent(messageId, s"${appName.toLowerCase}$deviceId", Channel.PUSH).get
-                      complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"Events fetched for messageId: $messageId contactId: $deviceId fetched.", Map(deviceId -> events.map(_._1)))))
-                    }
+            path(ChannelSegment / Segment / "messages" / Segment / Segment / "events") {
+              (channel: Channel, appName: String, deviceId: String, messageId: String) =>
+                get {
+                  meteredResource(s"reports${channel}MessageEvents.$appName") {
+                    val events = ServiceFactory.getCallbackService.fetchCallbackEvent(messageId, s"${appName.toLowerCase}$deviceId", channel).get
+                    complete(GenericResponse(StatusCodes.OK.intValue, null, Response(s"Events fetched for messageId: $messageId contactId: $deviceId fetched.", Map(deviceId -> events.map(_._1)))))
                   }
-              } ~ path(Segment / "messages" / Segment) {
-                (appName: String, contactId: String) =>
-                  get {
-                    meteredResource(s"reportsPushContactEvents.$appName") {
-                      parameters("startTs" ? 0L) { (startTs) =>
-                        val events = ServiceFactory.getCallbackService.fetchCallbackEventByContactId(s"${appName.toLowerCase}$contactId", Channel.PUSH, startTs, System.currentTimeMillis()).getOrElse(Nil)
-                        val messages = events.flatMap(e => ServiceFactory.getMessageService(Channel.PUSH).getRequestInfo(e._1.asInstanceOf[PNCallbackEvent].messageId).getOrElse(None))
-                        val finalTs = events.map(_._2).reduceLeftOption(_ max _).getOrElse(System.currentTimeMillis)
-
-                        complete(GenericResponse(StatusCodes.OK.intValue, Map("contactId" -> contactId, "appName" -> appName, "startTs" -> startTs), Response(s"messages fetched for $appName / $contactId", Map("messages" -> messages, "endTs" -> finalTs, "count" -> messages.size))))
+                }
+            } ~ path(ChannelSegment / Segment / "messages" / Segment) {
+              (channel: Channel, appName: String, contactId: String) =>
+                get {
+                  meteredResource(s"reports${channel}ContactEvents.$appName") {
+                    parameters("startTs" ? 0L) { (startTs) =>
+                      val events = ServiceFactory.getCallbackService.fetchCallbackEventByContactId(s"${appName.toLowerCase}$contactId", channel, startTs, System.currentTimeMillis()).getOrElse(Nil)
+                      val messages = channel match {
+                        case Channel.EMAIL => events.flatMap(e => ServiceFactory.getMessageService(channel).getRequestInfo(e._1.asInstanceOf[EmailCallbackEvent].messageId).getOrElse(None))
+                        case Channel.SMS => events.flatMap(e => ServiceFactory.getMessageService(channel).getRequestInfo(e._1.asInstanceOf[SmsCallbackEvent].messageId).getOrElse(None))
+                        case Channel.PUSH => events.flatMap(e => ServiceFactory.getMessageService(channel).getRequestInfo(e._1.asInstanceOf[PNCallbackEvent].messageId).getOrElse(None))
                       }
+                      val finalTs = events.map(_._2).reduceLeftOption(_ max _).getOrElse(System.currentTimeMillis)
+
+                      complete(GenericResponse(StatusCodes.OK.intValue, Map("contactId" -> contactId, "appName" -> appName, "startTs" -> startTs), Response(s"messages fetched for $appName / $contactId", Map("messages" -> messages, "endTs" -> finalTs, "count" -> messages.size))))
                     }
                   }
-              }
+                }
             } ~ path(ChannelSegment / "messages" / Segment / Segment / "events") {
               (channel: Channel, contactId: String, messageId: String) =>
                 get {
@@ -91,7 +93,7 @@ class ReportsRoute(implicit am: ActorMaterializer) extends BaseJsonHandler {
                   authorize(user, s"STATS_$clientId", "STATS") {
                     require(DateTimeUtils.parseCalendarDate(date).isSuccess, "Only 'yyyyMMdd' (Calendar Date) format allowed in `date`")
 
-                    val result = ServiceFactory.getReportingService.getAllDetails( date = date, clientId = clientId, contextId = contextId, stencilId = stencilId,  appName = appName, platform = None, channel =  None)
+                    val result = ServiceFactory.getReportingService.getAllDetails(date = date, clientId = clientId, contextId = contextId, stencilId = stencilId, appName = appName, platform = None, channel = None)
                     complete(GenericResponse(StatusCodes.OK.intValue, null, Response("Statistics", result)))
                   }
                 }
