5cddf14f3a8c48f9208f35dfbec6fc17f013e796
Kinshuk Bairagi <me@kinshuk.in>
2017-03-15 14:29:19 +0530

    Fix email with stencils and attachments

--- busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/RenderFlow.scala
+++ busybees/src/main/scala/com/flipkart/connekt/busybees/streams/flows/RenderFlow.scala
@@ -22,23 +22,23 @@ import com.flipkart.connekt.commons.utils.StringUtils._
 
 class RenderFlow extends MapFlowStage[ConnektRequest, ConnektRequest] with Instrumented {
 
-  lazy implicit val stencilService = ServiceFactory.getStencilService
+  private lazy implicit val stencilService = ServiceFactory.getStencilService
 
   override val map: (ConnektRequest) => List[ConnektRequest] = input => profile("map"){
     try {
       ConnektLogger(LogFile.PROCESSORS).debug("RenderFlow received message: {}", supplier(input.id))
       ConnektLogger(LogFile.PROCESSORS).trace("RenderFlow received message: {}", supplier(input.getJson))
 
-      val mRendered = input.copy(channelData = Option(input.channelData) match {
-        case Some(cD) => cD
-        case None => input.getComputedChannelData
+      val mRendered = input.copy(channelData = input.stencilId match {
+        case Some(_) => input.getComputedChannelData
+        case None => input.channelData
       }, meta = input.meta ++ input.stencilId.map("stencilId" -> _).toMap)
 
       List(mRendered)
     } catch {
       case e: Throwable =>
         ConnektLogger(LogFile.PROCESSORS).error(s"RenderFlow error", e)
-        throw new ConnektStageException(input.id, input.clientId,input.destinations, InternalStatus.RenderFailure, input.appName, input.channel,  input.contextId.orEmpty,  input.meta ++ input.stencilId.map("stencilId" -> _).toMap, s"RenderFlow-${e.getMessage}", e)
+        throw ConnektStageException(input.id, input.clientId,input.destinations, InternalStatus.RenderFailure, input.appName, input.channel,  input.contextId.orEmpty,  input.meta ++ input.stencilId.map("stencilId" -> _).toMap, s"RenderFlow-${e.getMessage}", e)
     }
   }
 }
