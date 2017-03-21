4661e5bd975e9df4d36986fb0eaa2e59cf972b05
Kinshuk Bairagi <me@kinshuk.in>
2017-03-22 03:21:35 +0530

    Update testcase

--- commons/src/test/scala/com/flipkart/connekt/commons/tests/dao/MessageQueueDaoTest.scala
+++ commons/src/test/scala/com/flipkart/connekt/commons/tests/dao/MessageQueueDaoTest.scala
@@ -24,7 +24,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 
 class MessageQueueDaoTest extends CommonsBaseTest {
 
-  private val contactId = "cae73a32-cc99-429e-b024-aab8aeob5"
+  private val contactId = UUID.randomUUID().toString
   private lazy val dao = DaoFactory.getMessageQueueDao
 
   override def beforeAll() = {
@@ -39,10 +39,9 @@ class MessageQueueDaoTest extends CommonsBaseTest {
     )
 
     val fetch = Await.result( dao.getMessages("testApp",contactId, None), 30.seconds)
-    println(fetch.size)
     println(fetch)
 
-    fetch.nonEmpty shouldEqual true
+    fetch.size shouldEqual 10
 
 
     println(Await.result( dao.trimMessages("testApp",contactId, 2), 30.seconds))
@@ -51,8 +50,10 @@ class MessageQueueDaoTest extends CommonsBaseTest {
     println(Await.result( dao.trimMessages("testApp",contactId, 2), 30.seconds))
 
 
-    println(Await.result( dao.getMessages("testApp",contactId, None), 30.seconds))
+    val fetch2 = Await.result( dao.getMessages("testApp",contactId, None), 30.seconds)
+    println(fetch2)
 
+    fetch2.size shouldEqual 2
 
   }
 
