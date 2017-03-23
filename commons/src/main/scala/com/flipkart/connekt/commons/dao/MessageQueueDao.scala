6d0ac022700d15651f4796a54c018a462fbb2caa
Kinshuk Bairagi <me@kinshuk.in>
2017-03-23 13:18:55 +0530

    Refactor QueueMetaData to caseClass

--- commons/src/main/scala/com/flipkart/connekt/commons/dao/MessageQueueDao.scala
+++ commons/src/main/scala/com/flipkart/connekt/commons/dao/MessageQueueDao.scala
@@ -20,6 +20,17 @@ import scala.collection.JavaConverters._
 import scala.concurrent.duration._
 import scala.concurrent.{ExecutionContext, Future}
 
+sealed case class MessageMetaData(createTs:Long, expiryTs:Long) {
+  def encoded:String = s"$createTs|$expiryTs"
+}
+
+private object MessageMetaData {
+  def apply(encoded:String): MessageMetaData = {
+    val parts = encoded.split('|').map(_.toLong)
+    MessageMetaData(parts.head, parts.last)
+  }
+}
+
 class MessageQueueDao(private val setName: String, private implicit val client: AsyncClient) extends Dao with AeroSpikeDao {
 
   private val namespace: String = "connekt"
@@ -28,7 +39,7 @@ class MessageQueueDao(private val setName: String, private implicit val client: 
 
   def enqueueMessage(appName: String, contactIdentifier: String, messageId: String, expiryTs: Long)(implicit ec: ExecutionContext): Future[Int] = {
     val key = new Key(namespace, setName, s"$appName$contactIdentifier")
-    val data = Map(messageId -> s"${System.currentTimeMillis()}|$expiryTs")
+    val data = Map(messageId -> MessageMetaData(System.currentTimeMillis(),expiryTs).encoded)
     addMapRow(key, binName, data, rowTTL).map { _record =>
       _record.getInt(binName)
     }
@@ -51,23 +62,25 @@ class MessageQueueDao(private val setName: String, private implicit val client: 
       case less if less.size <= numToRemove =>
         deleteRow(key).map(_ => 0)
       case normal =>
-        val sortedMessageIds = normal.toSeq.sortBy { case (_, data) => data.split('|').head.toLong }
+        val sortedMessageIds = normal.toSeq.sortBy { case (_, metadata) => metadata.createTs }
         val mIds = sortedMessageIds.take(numToRemove).map { case (messageId, _) => messageId }
         deleteMapRowItems(key, binName, mIds.toList).map(_ => normal.size - numToRemove)
     }
   }
 
-  private def getQueue(appName: String, contactIdentifier: String)(implicit ec: ExecutionContext): Future[Map[String, String]] = {
+  private def getQueue(appName: String, contactIdentifier: String)(implicit ec: ExecutionContext): Future[Map[String, MessageMetaData]] = {
     val key = new Key(namespace, setName, s"$appName$contactIdentifier")
     getRow(key).map { _record =>
       Option(_record).map { record =>
-        val (valid, expired) = record.getMap(binName).asScala.toMap.asInstanceOf[Map[String,String]].partition { case (_, data) =>
-          data.split('|').last.toLong >= System.currentTimeMillis()
+        val (valid, expired) = record.getMap(binName).asScala.toMap.asInstanceOf[Map[String,String]].map{ case (messageId, encodedMetaData) =>
+          messageId -> MessageMetaData(encodedMetaData)
+        }.partition { case (_, metadata) =>
+          metadata.expiryTs >= System.currentTimeMillis()
         }
         if (expired.nonEmpty)
           deleteMapRowItems(key, binName, expired.keys.toList)
         valid
-      } getOrElse Map.empty[String, String]
+      } getOrElse Map.empty[String, MessageMetaData]
     }
   }
 
@@ -79,13 +92,12 @@ class MessageQueueDao(private val setName: String, private implicit val client: 
     getQueue(appName, contactIdentifier).map { _messages =>
       val messages = timestampRange match {
         case Some((fromTs, endTs)) =>
-          _messages.filter { case (_, data) =>
-            val createTs = data.split('|').head.toLong
-            createTs >= fromTs && createTs <= endTs
+          _messages.filter { case (_, metadata) =>
+            metadata.createTs >= fromTs && metadata.createTs <= endTs
           }
         case None => _messages
       }
-      messages.toSeq.sortWith(_._2.split('|').head.toLong > _._2.split('|').head.toLong).map(_._1)
+      messages.toSeq.sortWith(_._2.createTs > _._2.createTs).map(_._1)
     }
   }
 
