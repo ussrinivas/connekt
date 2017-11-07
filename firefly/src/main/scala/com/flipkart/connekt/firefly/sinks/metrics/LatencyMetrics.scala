0db1705f160bcef2203dfc3692b3d636b482d250
grishma14m <grishma.s@flipkart.com>
2017-11-07 15:48:56 +0530

    CET-982: Taking time window instead of histogram

--- firefly/src/main/scala/com/flipkart/connekt/firefly/sinks/metrics/LatencyMetrics.scala
+++ firefly/src/main/scala/com/flipkart/connekt/firefly/sinks/metrics/LatencyMetrics.scala
@@ -12,21 +12,31 @@
  */
 package com.flipkart.connekt.firefly.sinks.metrics
 
+import java.util.concurrent.{ConcurrentHashMap, TimeUnit}
+
 import akka.Done
 import akka.stream.scaladsl.Sink
+import com.codahale.metrics.{SlidingTimeWindowReservoir, Timer}
 import com.flipkart.connekt.commons.entities.Channel
-import com.flipkart.connekt.commons.factories.{ConnektLogger, LogFile, ServiceFactory}
+import com.flipkart.connekt.commons.factories.ServiceFactory
 import com.flipkart.connekt.commons.iomodels._
 import com.flipkart.connekt.commons.metrics.Instrumented
 import com.flipkart.connekt.commons.services.ConnektConfig
 import com.flipkart.connekt.commons.utils.StringUtils._
 
+import scala.collection.JavaConverters._
 import scala.concurrent.Future
-
 
 class LatencyMetrics extends Instrumented {
 
   private def publishSMSLatency: Boolean = ConnektConfig.getBoolean("publish.sms.latency").getOrElse(true)
+
+  private val _timer = new ConcurrentHashMap[String,Timer]().asScala
+  private def slidingTimer(name:String):Timer =  _timer.getOrElseUpdate(name, {
+    val slidingTimer = new Timer(new SlidingTimeWindowReservoir(5, TimeUnit.MINUTES))
+    registry.register(name, slidingTimer)
+    slidingTimer
+  })
 
   def sink: Sink[CallbackEvent, Future[Done]] = Sink.foreach[CallbackEvent] {
     case event@(sce: SmsCallbackEvent) =>
@@ -41,7 +51,7 @@ class LatencyMetrics extends Instrumented {
             val receivedEvent = eventDetail.filter(_.eventType.equalsIgnoreCase("sms_received")).head
             val recTS = receivedEvent.asInstanceOf[SmsCallbackEvent].timestamp
             val lagTS = sce.timestamp - recTS
-            registry.histogram(getMetricName(s"SMS.latency.$providerName")).update(lagTS)
+            slidingTimer(getMetricName(s"SMS.latency.$providerName")).update(lagTS, TimeUnit.MILLISECONDS)
           }
         })
       }
