98cd4fcfb617a350f95d18785472a2c61362130d
grishma14m <grishma.s@flipkart.com>
2018-01-04 16:47:52 +0530

    CET-1085: Removing valid numbers

--- firefly/src/test/scala/com/flipkart/connekt/callback/topologies/LatencyMetricsTopologyTest.scala
+++ firefly/src/test/scala/com/flipkart/connekt/callback/topologies/LatencyMetricsTopologyTest.scala
@@ -11,7 +11,7 @@ class LatencyMetricsTopologyTest extends TopologyUTSpec with Instrumented {
   "HbaseLookupTopology Test" should "run" in {
     val smsCallback = SmsCallbackEvent(messageId = "c37d3855-c349-48c9-b3af-724eade554f0",
       eventType = "sms_delivered",
-      receiver = "+918100989832",
+      receiver = "+911234567843",
       clientId = "affordability",
       appName = "flipkart",
       contextId = "",
@@ -25,7 +25,7 @@ class LatencyMetricsTopologyTest extends TopologyUTSpec with Instrumented {
   "HbaseLookupTopology Test" should "cargo blank" in {
     val smsCallback = SmsCallbackEvent(messageId = "62b7d6a1-cdb8-414d-8954-972bae4aec2c",
       eventType = "CLICK",
-      receiver = "+918885472168",
+      receiver = "+911234567843",
       clientId = "affordability",
       appName = "flipkart",
       contextId = "",
@@ -39,7 +39,7 @@ class LatencyMetricsTopologyTest extends TopologyUTSpec with Instrumented {
   "HbaseLookupTopology Test" should "cargo any" in {
     val smsCallback = SmsCallbackEvent(messageId = "089dae77-051d-4146-ad25-ea86ed761608",
       eventType = "sms_delivered",
-      receiver = "+918885472168",
+      receiver = "+911234567843",
       clientId = "affordability",
       appName = "flipkart",
       contextId = "",
@@ -54,7 +54,7 @@ class LatencyMetricsTopologyTest extends TopologyUTSpec with Instrumented {
   "HbaseLookupTopology Test" should "cargo excssep" in {
     val smsCallback = SmsCallbackEvent(messageId = "1c4f3564-3687-4f72-a43d-f75dd111d0",
       eventType = "sms_received",
-      receiver = "+918885472168",
+      receiver = "+911234567843",
       clientId = "affordability",
       appName = "flipkart",
       contextId = "",
@@ -69,7 +69,7 @@ class LatencyMetricsTopologyTest extends TopologyUTSpec with Instrumented {
   "False DeliveredTS Test" should "run" in {
     val smsCallback = SmsCallbackEvent(messageId = "3ce2de85-be4f-4ccb-a91b-1ce8b424ab4c",
       eventType = "sms_delivered",
-      receiver = "+918885472168",
+      receiver = "+911234567843",
       clientId = "affordability",
       appName = "flipkart",
       contextId = "",
