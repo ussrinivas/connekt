71e86b0f4b9c803f4a100571a35cde3849467d10
ankitgupta027 <ankit.gs@flipkart.com>
2018-01-12 19:09:11 +0530

    CET-1169 Making suggested changes and moving logic to check whatsapp contact in WACheckContactHelper.

--- receptors/src/main/scala/com/flipkart/connekt/receptors/routes/helper/WAContactCheckHelper.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/routes/helper/WAContactCheckHelper.scala
@@ -32,6 +32,25 @@ object WAContactCheckHelper extends Instrumented {
 
   private val baseUrl = ConnektConfig.getString("wa.base.uri").get
   private val timeout = ConnektConfig.getInt("wa.contact.api.timeout").get.seconds
+  private val checkContactInterval = ConnektConfig.getInt("wa.check.contact.interval.days").get
+
+  //returns valid tuple of valid numbers list and invalid numbers list
+  def checkContact(appName: String, destinations: Set[String])(implicit am: ActorMaterializer): (List[WAContactEntity], List[WAContactEntity]) = {
+    val validWAUsers = ListBuffer[WAContactEntity]()
+    val invalidWAUsers = ListBuffer[WAContactEntity]()
+    val withinTTLWAList = WAContactService.instance.gets(appName, destinations.toSet) match {
+      case Success(waList) => waList.filter(System.currentTimeMillis() - _.lastCheckContactTS < checkContactInterval.days.toMillis)
+      case _ => Nil
+    }
+    (checkContactViaWAApi(destinations.toList.diff(withinTTLWAList.map(_.destination)), appName) ++ withinTTLWAList)
+      .map(wa =>
+        if (wa.exists.toLowerCase.contains("true"))
+          validWAUsers += wa
+        else
+          invalidWAUsers += wa
+      )
+    (validWAUsers.toList, invalidWAUsers.toList)
+  }
 
   def checkContactViaWAApi(destinations: List[String], appName: String)(implicit am: ActorMaterializer): List[WAContactEntity] = {
     if (destinations.nonEmpty) {
--- receptors/src/main/scala/com/flipkart/connekt/receptors/routes/master/WASubscriptionRoute.scala
+++ receptors/src/main/scala/com/flipkart/connekt/receptors/routes/master/WASubscriptionRoute.scala
@@ -18,12 +18,11 @@ import akka.stream.ActorMaterializer
 import com.flipkart.connekt.commons.entities.Channel
 import com.flipkart.connekt.commons.factories.{ConnektLogger, LogFile, ServiceFactory}
 import com.flipkart.connekt.commons.iomodels._
-import com.flipkart.connekt.commons.services.{ConnektConfig, GuardrailService, WAContactService}
+import com.flipkart.connekt.commons.services.{ConnektConfig, GuardrailService}
 import com.flipkart.connekt.commons.utils.StringUtils
 import com.flipkart.connekt.receptors.routes.BaseJsonHandler
 import com.flipkart.connekt.commons.utils.StringUtils._
 import com.flipkart.connekt.receptors.routes.helper.{PhoneNumberHelper, WAContactCheckHelper}
-import scala.concurrent.duration._
 import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
@@ -37,54 +36,50 @@ class WASubscriptionRoute (implicit am: ActorMaterializer) extends BaseJsonHandl
     authenticate {
       user =>
         pathPrefix("v1") {
-          pathPrefix("whatsapp" / "subscription" / Segment) {
-            (appName: String) =>
-              pathPrefix(Segment) {
-                (destination: String) =>
-                  pathEndOrSingleSlash {
-                    put {
-                      meteredResource("waSubscription") {
-                        authorize(user, "WA_SUBSCRIPTION", s"WA_SUBSCRIPTION_$appName") {
-                          entity(as[WASubscriptionRequest]) { subRequest =>
-                            complete {
-                              Future {
-                                PhoneNumberHelper.validateNFormatNumber(appName, destination) match {
-                                  case Some(validNumber) =>
-                                    GuardrailService.upsertSubscription[String, Any](appName, Channel.WA, validNumber, subRequest.asMap.asInstanceOf[Map[String, AnyRef]]) match {
-                                      case Success(subResp) =>
-                                        if (subResp.asInstanceOf[Map[String,Any]]("firstTimeUser").toString.toBoolean) {
-                                          val number = WAContactService.instance.get(appName, destination).get match {
-                                            case Some(wa) if (System.currentTimeMillis() - wa.lastCheckContactTS) < checkContactInterval.days.toMillis =>
-                                              wa.userName
-                                            case _ =>
-                                              val contactWAStatus = WAContactCheckHelper.checkContactViaWAApi(List(destination), appName)
-                                              contactWAStatus.head.userName
-                                          }
-                                          val channelInfo = WARequestInfo(appName = appName, destinations = Set(number))
+          pathPrefix("whatsapp" / "subscription" / Segment / Segment) {
+            (appName: String, destination: String) =>
+              pathEndOrSingleSlash {
+                put {
+                  meteredResource("waSubscription") {
+                    authorize(user, "WA_SUBSCRIPTION", s"WA_SUBSCRIPTION_$appName") {
+                      entity(as[WASubscriptionRequest]) { subRequest =>
+                        complete {
+                          Future {
+                            PhoneNumberHelper.validateNFormatNumber(appName, destination) match {
+                              case Some(validNumber) =>
+                                GuardrailService.modifyGuard[String, Any, Map[String, Boolean]](appName, Channel.WA, validNumber, subRequest.asMap.asInstanceOf[Map[String, AnyRef]]) match {
+                                  case Success(subResp) =>
+                                    if (subResp("firstTimeUser")) {
+                                      val checkedContacts = WAContactCheckHelper.checkContact(appName, Set(validNumber))
+                                      val number = checkedContacts._1.headOption match {
+                                        case Some(n) =>
+                                          val channelInfo = WARequestInfo(appName = appName, destinations = Set(n.userName))
                                           val channelData = WARequestData(waType = WAType.hsm)
                                           val channelDataModel = StringUtils.getObjectNode
                                           channelDataModel.put("pickupTime", "10AM")
                                           channelDataModel.put("orderId", "ankit-order-id")
-                                          val meta = Map("bucket" -> "transactional", "subBucket" -> "orderRelated", "domain" -> "flipkart")
-                                          val connektRequest = new ConnektRequest(generateUUID, "whatspp", Some("WELCOME"), "wa", "H", Some(WELCOME_STENCIL), None, None, channelInfo, channelData, channelDataModel, meta)
+                                          val meta = WAMetaData("flipkart", "transactional", "order").asMap.asInstanceOf[Map[String, String]]
+                                          val connektRequest = ConnektRequest(generateUUID, "whatspp", Some("WELCOME"), "wa", "H", Some(WELCOME_STENCIL), None, None, channelInfo, channelData, channelDataModel, meta)
                                           val queueName = ServiceFactory.getMessageService(Channel.WA).getRequestBucket(connektRequest, user)
-                                          ServiceFactory.getMessageService(Channel.WA).saveRequest(connektRequest, queueName, persistPayloadInDataStore = true)
-                                        }
-                                        GenericResponse(StatusCodes.OK.intValue, null, Response(s"WA subscription has been set to : ${subRequest.subscription}, for destination : $validNumber", null))
-                                      case Failure(f) =>
-                                        ConnektLogger(LogFile.SERVICE).error(s"Subscription update Failed ")
-                                        GenericResponse(StatusCodes.BadRequest.intValue, null, Response("Subscription update Failed.", f))
+                                          ServiceFactory.getMessageService(Channel.WA).saveRequest(connektRequest, queueName)
+                                        case None =>
+                                          ConnektLogger(LogFile.SERVICE).error(s"User is not available in whatsapp, $validNumber ")
+                                      }
                                     }
-                                  case None =>
-                                    GenericResponse(StatusCodes.BadRequest.intValue, null, Response("No valid destinations found", destination))
+                                    GenericResponse(StatusCodes.OK.intValue, null, Response(s"WA subscription has been set to : ${subRequest.subscription}, for destination : $validNumber", null))
+                                  case Failure(f) =>
+                                    ConnektLogger(LogFile.SERVICE).error(s"Subscription update Failed ")
+                                    GenericResponse(StatusCodes.BadRequest.intValue, null, Response("Subscription update Failed.", f))
                                 }
-                              }(ioDispatcher)
+                              case None =>
+                                GenericResponse(StatusCodes.BadRequest.intValue, null, Response("No valid destinations found", destination))
                             }
-                          }
+                          }(ioDispatcher)
                         }
                       }
                     }
                   }
+                }
               }
           }
         }
