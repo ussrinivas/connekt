e1f805a4dec584ee55b91514d7e9b75c13c064a2
Hirendra Singh Thakur <hirendra99549@users.noreply.github.com>
2018-02-16 18:31:31 +0530

    removed wns from PNMessagingCarrier

--- commons/src/main/scala/com/flipkart/connekt/commons/entities/PNMessagingCarrier.scala
+++ commons/src/main/scala/com/flipkart/connekt/commons/entities/PNMessagingCarrier.scala
@@ -2,8 +2,6 @@ package com.flipkart.connekt.commons.entities
 
 object PNMessagingCarrier extends Enumeration {
   type PNMessagingCarrier = Value
-
   val FCM = Value("fcm")
   val APN = Value("apn")
-  val WNS = Value("wns")
 }
